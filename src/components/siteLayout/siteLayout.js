b13b67e15273bbf0ccd9672e240c3cc77d1ad610
Brett Francis <brett.francis@kc.frb.org>
2019-05-24 13:25:39 -0500

    favicon

--- src/components/siteLayout/siteLayout.js
+++ src/components/siteLayout/siteLayout.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react"
+import React from "react"
 import './main.scss';
 import SiteFooter from "../footer/footer";
 import SiteHeader from "../siteHeader/header";
+import Helmet from 'react-helmet'
 
 export default ({ children }) => (
     <>
+        <Helmet>
+            <link rel="shortcut icon" href="/favicon.ico" />
+        </Helmet>
+
         <SiteHeader />
         <article id="main">
-        {children}
+            {children}
         </article>
         <SiteFooter />
     </>
