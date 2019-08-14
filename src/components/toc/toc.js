62d25975d0db4fe55e529b66e165da89fb529544
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-08-13 23:26:35 -0400

    styled toc

--- src/components/toc/toc.js
+++ src/components/toc/toc.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./toc.scss"
 
-class Toc extends Component {
+const Toc = (props) => {
+  return (
+    <div className="container">
+      <div className="row center-lg">
+        <div className="col-lg-10">
+          <section id="TOC">
+            <div className="row">
+              {
+                props.sections.map((item, key) =>
+                  <div key={key} className="col-lg-3 col-md-6 col-xs-12 toc__tile ">
+                    <a href={`#section-${item.section}`}>
+                      <div className="toc__number">
+                        <h2>
+                          {item.number}
+                        </h2>
+                      </div>
+                      <h4 className="toc__subtext">
+                        {item.subtext}
+                      </h4>
+                      <h3 className="toc__subblurb">
+                        {item.subblurb}
+                      </h3>
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row center-lg">
-          <div className="col-lg-10">
-            <section id="TOC">
-              <div className="row">
-                {
-                  this.props.sections.map((item, key) =>
-                    <div key={key} className="toc__tile col-lg-3 col-md-6 col-xs-12">
-                      <a href={`#section-${item.section}`}>
-                        <div className="toc--number">
-                          <h2>
-                            {item.number}
-                          </h2>
-                        </div>
-                        <h4 className="toc--subtext">
-                          {item.subtext}
-                        </h4>
-                        <h3 className="toc--subblurb">
-                          {item.subblurb}
-                        </h3>
-
-                        <p className="toc--blurb">
-                          {item.blurb}
-                        </p>
-                      </a>
-                    </div>
-                  )
-                }
-              </div>
-            </section>
-          </div>
+                      <p className="toc__blurb">
+                        {item.blurb}
+                      </p>
+                    </a>
+                  </div>
+                )
+              }
+            </div>
+          </section>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 
 export default Toc;
