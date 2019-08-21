1678065f2960848c0180f61802328b6c018d69ba
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-08-21 15:48:45 -0400

    adding resources

--- src/page-sections/home/resources/resources.jsx
+++ src/page-sections/home/resources/resources.jsx
@@ -1,68 +1,66 @@
 import React from 'react';
 import { Link } from "gatsby"
+import apiImg from '../../../images/home/resources-api-2.svg'
+import userGuideImg from '../../../images/home/resources-user-guide-2.svg'
+import playbookImg from '../../../images/home/resources-playbook-2.svg'
+import './resources.scss'
 
 const Resources = (props) => {
+
+  const resources = [{
+    href: 'http://api.usaspending.gov/',
+    imageSrc: apiImg,
+    title: 'API Guide',
+    description: 'Guidance on accessing Treasury data with open source code.'
+    }, {
+      href: '/assets/analyst-guide-1-2.pdf',
+      imageSrc: userGuideImg,
+      title: 'Analyst Guide',
+      description: 'Methods to easily navigate data from our sister site, USAspending.gov'
+    }, {
+      href: '#',
+      imageSrc: playbookImg,
+      title: 'Student Innovators Toolbox',
+      description: 'Ways for professors and students to get involved.'
+    },
+  ]
+
   return (
     <>
-      <section className="landing-row landing-row--resources">
-        <h1 className="landing-row__heading">
-          Resources
-        </h1>
-        <div className="landing-row__tiles">
-
-          <div className="landing-row__tile landing-row__tile--primary">
-            <div className="landing-row__text">
-              <p className="landing-row__title">
-                Do you want
-                <strong> to conduct your own analysis?</strong>
-              </p>
-              <p className="landing-row__p">
-                Access Treasury data and create your own charts and visualizations!
-              </p>
-            </div>
+      <div className="landing-row--resources">
+        <div className="row">
+          <div class="col-xs-12">
+            <h1 className="landing-row__heading">
+              Resources
+            </h1>
           </div>
-
-          <a className="landing-row__tile" href="http://api.usaspending.gov/">
-            <img src="../images/home/resources-api-2.svg" className="svgImg"/>
-            <div className="landing-row__text">
-              <h1 className="landing-row__title landing-row__title-mod">
-                API Guide
-              </h1>
-              <p className="landing-row__p landing-row__p--hide">
-                Guidance on accessing Treasury data with open source code.
-              </p>
-            </div>
-          </a>
-
-          <a className="landing-row__tile" href="/assets/analyst-guide-1-2.pdf">
-            <img src="../images/home/resources-user-guide-2.svg" className="svgImg"/>
-            <div className="landing-row__text">
-              <h1 className="landing-row__title landing-row__title-mod">
-                Analyst Guide
-              </h1>
-              <p className="landing-row__p landing-row__p--hide">
-                Methods to easily navigate data from our sister site, USAspending.gov
-              </p>
-            </div>
-          </a>
-
-          <a className="landing-row__tile landing-row__tile--last" href="/student-innovators-toolbox.html">
-            <img src="../images/home/resources-playbook-2.svg" className="svgImg"/>
-            <div className="landing-row__text">
-              <h1 className="landing-row__title landing-row__title-mod">
-                Student Innovators Toolbox
-              </h1>
-              <p className="landing-row__p landing-row__p--hide">
-                Ways for professors and students to get involved.
-              </p>
-            </div>
-          </a>
-
-
         </div>
-
-
-      </section>
+        <div className="row">
+          <div className="landing-row__tile col-xs-3">
+            <p className="landing-row__title">
+              Do you want
+              <strong> to conduct your own analysis?</strong>
+            </p>
+            <p className="landing-row__p">
+              Access Treasury data and create your own charts and visualizations!
+            </p>
+          </div>
+          {/*// add conditional here */}
+          {/*//if last add class*/}
+          {resources.map((resource) =>
+            <a className="landing-row__tile col-xs-3 center-xs"
+              href={resource.href}>
+              <img src={resource.imageSrc} className="svgImg"/>
+              <h1 className="landing-row__title">
+                {resource.title}
+              </h1>
+              <p className="landing-row__p">
+                {resource.description}
+              </p>
+            </a>
+          )}
+        </div>
+      </div>
     </>
   )
 }
--- src/page-sections/home/tertiary-analyses/tertiary-analyses.jsx
+++ src/page-sections/home/tertiary-analyses/tertiary-analyses.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Link } from "gatsby"
 import "./tertiary-analyses.scss"
+import homelessImg from "../../../images/home/homelessness.png"
 
 const TertiaryAnalyses = (props) => {
   return (
     <>
-      <a href="/homelessness-analysis.html"
-         ga-on="click" ga-event-category="Data Lab Home Page" ga-event-action="Clicked 'Homelessness Analysis'"
-      >
-        <div className="feature-row__border">
-          <div className="feature-row__text">
-            <h1 className="feature-row__heading">
-              Homelessness Analysis
-            </h1>
-            <p className="feature-row__title">
-              Explore federal programs that address homelessness
-            </p>
-            <p className="feature-row__blurb">
-              On a single night in 2016, more than 500,000 people experienced homelessness in the United States. With
-              our
-              visualizations, you can see which federal programs are addressing homelessness, the amount of federal
-              spending on homelessness by region, and where individuals experiencing homelessness are located. We also
-              provide information about which regions are similar to others based on their homeless populations and
-              funding profiles.
-            </p>
+      <div className="tertiary">
+        <Link to="#" href="/homelessness-analysis.html"
+           ga-on="click" ga-event-category="Data Lab Home Page" ga-event-action="Clicked 'Homelessness Analysis'"
+        >
+          <div className="tertiary__border">
+            <div className="tertiary__text">
+              <h1 className="tertiary__heading">
+                Homelessness Analysis
+              </h1>
+              <p className="tertiary__title">
+                Explore federal programs that address homelessness
+              </p>
+              <p className="tertiary__blurb">
+                On a single night in 2016, more than 500,000 people experienced homelessness in the United States. With
+                our
+                visualizations, you can see which federal programs are addressing homelessness, the amount of federal
+                spending on homelessness by region, and where individuals experiencing homelessness are located. We also
+                provide information about which regions are similar to others based on their homeless populations and
+                funding profiles.
+              </p>
+            </div>
+            <div className="tertiary__image">
+              <img src={homelessImg} />
+            </div>
           </div>
-          <div className="feature-row__image">
-            <img src="../images/home/homelessness.png"/>
-          </div>
-        </div>
-      </a>
+        </Link>
+      </div>
     </>
   )
 }
