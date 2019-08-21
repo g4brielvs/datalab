680a7af32fcedfc1f04e2b6fa40b589916927515
Brett Varney <brett.varney@kc.frb.org>
2019-08-21 08:55:13 -0500

    revert to component import I get rendering errors using ReactComponent

--- src/components/footers/page.jsx
+++ src/components/footers/page.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./page.scss"
 
 import Logo from "../logos/logo"
-import { ReactComponent as Github } from "../logos/github.svg"
+import Github from "../logos/github"
 import Dataworld from "../logos/dataworld"
 import Twitter from "../logos/twitter"
 import Facebook from "../logos/facebook"
