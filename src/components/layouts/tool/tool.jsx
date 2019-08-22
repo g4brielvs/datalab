61ded5174456c5b3ecce643a0f9bcafb17952180
Brett Varney <brett.varney@kc.frb.org>
2019-08-22 10:30:32 -0500

    wip adjust follow-up section

--- src/components/layouts/tool/tool.jsx
+++ src/components/layouts/tool/tool.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import './tool.scss'
 
 import ShareMenu from '../../share-menu/share-menu'
+import MoreAnalyses from '../../more-analyses/more-analyses'
 import PageFooter from '../../footers/page'
 import SiteFooter from '../../footers/site'
 
@@ -10,9 +11,7 @@ const ToolLayout = props => (
   <div className='page'>
     <header>
       <span className='title'>{props.title}</span>
-      <span>
-        <ShareMenu />
-      </span>
+      <span><ShareMenu /></span>
     </header>
     <section>
       <p className='intro'>{props.introSentence}</p>
@@ -24,18 +23,19 @@ const ToolLayout = props => (
         <section className='chart'>
           {props.children}
         </section>
-        <section className='follow-up container'>
+        <section className='follow-up container-fluid'>
           <div className='row justify-content-center'>
-            <div className='intro col-xs-4'>
+            <div className='intro col-xs-5'>
               {props.sectionTitle}
             </div>
-            <div className='col-xs-6'>
+            <div className='col-xs-7'>
               {props.sectionText}
             </div>
           </div>
         </section>
       </article>
     </main>
+    <MoreAnalyses />
     <PageFooter />
     <SiteFooter />
   </div>
