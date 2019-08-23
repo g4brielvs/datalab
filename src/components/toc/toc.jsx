3e64cfd40366d0ba679d819216c6e4caee76a7f2
Brett Varney <brett.varney@kc.frb.org>
2019-08-23 15:26:32 -0500

    wip testing rules

--- src/components/toc/toc.jsx
+++ src/components/toc/toc.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './toc.scss';
 
-const Toc = (props) => (
-  <div className="TOC container">
+const Toc = (props) =>
+	<div className="TOC container">
 		<div className="row center-lg">
 			<div className="col-lg-10">
 				<section id="TOC">
 					<div className="row">
 						{
-							props.sections.map((item, key) =>
+							props.sections.map((item, key) => {
 								<div key={key} className="col-lg-3 col-md-6 col-xs-12 toc__tile">
 									<a href={`#section-${item.section}`}>
 										<div className="toc__number">
@@ -28,14 +28,14 @@ const Toc = (props) => (
 										</p>
 									</a>
 								</div>
-							)
+							})
 						}
 					</div>
 				</section>
 			</div>
 		</div>
 	</div>
-)
+;
 
 
 export default Toc;
