a13a21bc62d7f248e01c2ba3b24015296c1b08fc
Brett Varney <brett.varney@kc.frb.org>
2019-08-29 15:05:58 -0500

    da4521 panel styled & positioned

--- src/components/chartpanels/search.jsx
+++ src/components/chartpanels/search.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import './search.scss';
+import React from "react"
+import "./search.scss"
 
-import Popover from '@material-ui/core/Popover';
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search"
+import BubbleChartOutlinedIcon from "@material-ui/icons/BubbleChartOutlined"
+import Divider from "@material-ui/core/Divider"
+import ListIcon from '@material-ui/icons/List';
+import Popover from "@material-ui/core/Popover"
 
 const SearchPanel = props => {
   return (
-    <>
-      <div className='search-panel'>
-        <SearchIcon />
+    <div className="search-panel">
+      <div className="panel-group">
+        <SearchIcon className="panel-button" />
+      </div>
+      <div className="panel-group">
+        <BubbleChartOutlinedIcon className="panel-button" />
+        <Divider variant="middle" className='divider' />
+        <ListIcon className="panel-button" />
       </div>
       {/* <Popover
       ></Popover> */}
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default SearchPanel;
+export default SearchPanel
