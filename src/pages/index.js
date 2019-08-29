d0cfdff5a3559218dc032bb91eafcd093e12615a
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-08-29 14:21:21 -0400

    adding main and section tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,22 +18,19 @@ import "./index.scss"
 import { Helmet } from "react-helmet"
 
 
+function IndexPage() {
 
-
-
-function IndexPage () {
-
-  let [d3, setD3] = useState(window.d3);
+  let [d3, setD3] = useState(window.d3)
 
 
   function handleScriptInject({ scriptTags }) {
     if (scriptTags) {
-      const scriptTag = scriptTags[0];
+      const scriptTag = scriptTags[0]
       scriptTag.onload = () => {
         // I don't really like referencing window.
-        console.log(`myExternalLib loaded!`, window.d3);
-        setD3(d3);
-      };
+        console.log(`myExternalLib loaded!`, window.d3)
+        setD3(d3)
+      }
     }
   }
 
@@ -45,93 +42,94 @@ function IndexPage () {
         onChangeClientState={(newState, addedTags) => handleScriptInject(addedTags)}
       />
 
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
 
-      <div className="container-fluid">
-          <div className="featured-tile-row row center-xs">
+      <main role="main">
+        <div className="container-fluid">
+          <section className="featured-tile-row row center-xs">
             <div className="col-lg-8 col-md-12 featured-tile">
-                <FeaturedAnalysesTile
-                  href={'colleges-and-universities'}
-                  imgSrc={main}
-                  imgAlt={'Answer all your questions about federal government finance'}
-                  heading={'Federal Investment in Higher Education'}
-                  body={`The Federal Investment in Higher Education analysis gives an overview of federal funding in colleges and
-                    universities through grants, contracts, and student aid. This interactive analysis gives you an
-                    opportunity to search schools and discover how much the government has invested in that institution. In
-                    the visualizations you can also break down the investment by federal agency and view data by investment
-                    category. This analysis gives the public an interactive lens into the government’s investment in higher
-                    education.`}
-                  mobileBody={`The Federal Investment in Higher Education analysis gives an overview of federal funding in colleges
-                    and universities. Dive in and search by individual schools, federal agencies, or investment
-                    categories!`
-                  } />
+              <FeaturedAnalysesTile
+                href={"colleges-and-universities"}
+                imgSrc={main}
+                imgAlt={"Answer all your questions about federal government finance"}
+                heading={"Federal Investment in Higher Education"}
+                body={`The Federal Investment in Higher Education analysis gives an overview of federal funding in colleges and
+										universities through grants, contracts, and student aid. This interactive analysis gives you an
+										opportunity to search schools and discover how much the government has invested in that institution. In
+										the visualizations you can also break down the investment by federal agency and view data by investment
+										category. This analysis gives the public an interactive lens into the government’s investment in higher
+										education.`}
+                mobileBody={`The Federal Investment in Higher Education analysis gives an overview of federal funding in colleges
+										and universities. Dive in and search by individual schools, federal agencies, or investment
+										categories!`
+                }/>
 
-          </div>
-
-          <div className="col-lg-4 col-md-12">
-          <DtsTile
-            href={'dts'}
-            heading={'Visualizing the Daily Treasury Statement'}
-            title={'How much does the federal government spend each day?'} />
-
-            <div className="row">
-              <div className="col-lg-12 col-md-6 col-sm-12">
-                <SecondaryAnalysesTile
-                  heading={'Contract Spending Analysis'}
-                  subheading={'How has federal contract spending changed over time?'}
-                  href={'contract-explorer.html'}
-                  imgSrc={contractSpending}
-                  imgAlt={' '}
-                  body={`We explore 10 years of contract data, and provide an analysis of the
-                  impact of short-term continuing resolutions on contract spending.`}
-                />
-              </div>
-
-              <div className="col-lg-12 col-md-6 col-sm-12">
-                <SecondaryAnalysesTile
-                  heading={'Contract Explorer'}
-                  subheading={'Who receives federal contracts?'}
-                  href={'contract-explorer.html'}
-                  imgSrc={contractExplorer}
-                  imgAlt={' '}
-                  body={`The federal government spends about $500 billion each year on contracts. Learn more
-                      about contract recipients and what agencies receive from these contracts.`}
-                />
-              </div>
             </div>
 
-          </div>
-        </div>
+            <div className="col-lg-4 col-md-12">
+              <DtsTile
+                href={"dts"}
+                heading={"Visualizing the Daily Treasury Statement"}
+                title={"How much does the federal government spend each day?"}/>
 
-        <div className="more-analyses-row row">
-          <div className="col-xs-12">
-            <MoreAnalysesRow/>
-          </div>
-        </div>
+              <div className="row">
+                <div className="col-lg-12 col-md-6 col-sm-12">
+                  <SecondaryAnalysesTile
+                    heading={"Contract Spending Analysis"}
+                    subheading={"How has federal contract spending changed over time?"}
+                    href={"contract-explorer.html"}
+                    imgSrc={contractSpending}
+                    imgAlt={" "}
+                    body={`We explore 10 years of contract data, and provide an analysis of the
+									impact of short-term continuing resolutions on contract spending.`}
+                  />
+                </div>
 
-        <div className="featured-row row">
-          <div className="col-xs-12">
-            <FeaturedAnalysesRow
-              href={'homelessness-analysis'}
-              heading={'Homelessness Analysis'}
-              title={'Explore federal programs that address homelessness'}
-              blurb={`On a single night in 2016, more than 500,000 people experienced homelessness in the United States. With
-                  our visualizations, you can see which federal programs are addressing homelessness, the amount of federal
-                  spending on homelessness by region, and where individuals experiencing homelessness are located. We also
-                  provide information about which regions are similar to others based on their homeless populations and
-                  funding profiles.`}
+                <div className="col-lg-12 col-md-6 col-sm-12">
+                  <SecondaryAnalysesTile
+                    heading={"Contract Explorer"}
+                    subheading={"Who receives federal contracts?"}
+                    href={"contract-explorer.html"}
+                    imgSrc={contractExplorer}
+                    imgAlt={" "}
+                    body={`The federal government spends about $500 billion each year on contracts. Learn more
+											about contract recipients and what agencies receive from these contracts.`}
+                  />
+                </div>
+              </div>
+
+            </div>
+          </section>
+
+          <section className="more-analyses-row row">
+            <div className="col-xs-12">
+              <MoreAnalysesRow/>
+            </div>
+          </section>
+
+          <section className="featured-row row">
+            <div className="col-xs-12">
+              <FeaturedAnalysesRow
+                href={"homelessness-analysis"}
+                heading={"Homelessness Analysis"}
+                title={"Explore federal programs that address homelessness"}
+                blurb={`On a single night in 2016, more than 500,000 people experienced homelessness in the United States. With
+									our visualizations, you can see which federal programs are addressing homelessness, the amount of federal
+									spending on homelessness by region, and where individuals experiencing homelessness are located. We also
+									provide information about which regions are similar to others based on their homeless populations and
+									funding profiles.`}
               />
-          </div>
+            </div>
+          </section>
+
+          <section className="resource-row row">
+            <div className="col-xs-12">
+              <ResourcesRow/>
+            </div>
+          </section>
+
         </div>
-
-
-        <div className="row margin-bottom">
-          <div className="col-xs-12">
-            <ResourcesRow/>
-          </div>
-        </div>
-
-      </div>
+      </main>
 
     </Home>
   )
