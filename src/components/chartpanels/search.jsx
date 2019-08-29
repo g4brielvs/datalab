125f4c45613f4a7b6d5447707eef48da7f877101
Brett Varney <brett.varney@kc.frb.org>
2019-08-29 15:31:15 -0500

    da4521 wip adding click fns

--- src/components/chartpanels/search.jsx
+++ src/components/chartpanels/search.jsx
@@ -1,27 +1,37 @@
 import React from "react"
 import "./search.scss"
 
+import IconButton from "@material-ui/core/IconButton"
 import SearchIcon from "@material-ui/icons/Search"
 import BubbleChartOutlinedIcon from "@material-ui/icons/BubbleChartOutlined"
 import Divider from "@material-ui/core/Divider"
-import ListIcon from '@material-ui/icons/List';
+import ListIcon from "@material-ui/icons/List"
 import Popover from "@material-ui/core/Popover"
 
-const SearchPanel = props => {
-  return (
-    <div className="search-panel">
-      <div className="panel-group">
-        <SearchIcon className="panel-button" />
-      </div>
-      <div className="panel-group">
-        <BubbleChartOutlinedIcon className="panel-button" />
-        <Divider variant="middle" className='divider' />
-        <ListIcon className="panel-button" />
-      </div>
-      {/* <Popover
-      ></Popover> */}
-    </div>
-  )
-}
+export default class SearchPanel extends React.Component {
 
-export default SearchPanel
+  search () {
+    alert('search clicked');
+  }
+
+  render() {
+    return (
+      <div className="search-panel">
+        <IconButton onClick={e => this.search()} className="panel-group" aria-label="search">
+          <SearchIcon className="panel-button" />
+        </IconButton>
+        <div className="panel-group">
+          <IconButton aria-label="show bubble chart">
+            <BubbleChartOutlinedIcon className="panel-button" />
+          </IconButton>
+          <Divider variant="middle" className="divider" />
+          <IconButton aria-label="show data table">
+            <ListIcon className="panel-button" />
+          </IconButton>
+        </div>
+        {/* <Popover
+        ></Popover> */}
+      </div>
+    )
+  }
+}
