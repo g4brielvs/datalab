8da14882d0bc9e1b30c8958909cef84de6de2a5c
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-08-30 13:34:55 -0400

    wip

--- src/pages/budget-function/index.jsx
+++ src/pages/budget-function/index.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from "react"
-import SEO from "../../components/seo"
+import { graphql } from "gatsby"
 
 
 class BudgetFunctionPage extends Component {
 
   render() {
+
+    // const data = this.props.data.allSankeyV1Fy19Csv.nodes;
+    // const sPanel = this.props.data.allSankeyPanelV1Fy19Csv.nodes;
+    // const sTitle = this.props.data.allSankeyTitlesV1Fy19Csv.nodes;
+    // const descriptions = this.props.data.allDescriptionsCsv.nodes;
+
     return <>
         <p>hello world!</p>
       </>
@@ -12,3 +18,35 @@ class BudgetFunctionPage extends Component {
 }
 
 export default BudgetFunctionPage
+
+
+export const IndexQuery = graphql`
+  query {
+    allSankeyV1Fy19Csv {
+      nodes {
+        source
+        target
+        value
+      }
+    },
+    allSankeyPanelV1Fy19Csv {
+      nodes {
+        source
+        target
+        value
+      }
+    },
+    allSankeyTitlesV1Fy19Csv {
+      nodes {
+        name
+        value
+      }
+    },
+     allDescriptionsCsv {
+      nodes {
+        name
+        desc
+      }
+    },
+  }
+`
