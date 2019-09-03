496e146cb45a7e6fe3d3f2aea2c9c124c3df9028
Brett Varney <brett.varney@kc.frb.org>
2019-09-03 11:04:34 -0500

    wip show/hide search panel working on highlighting current button

--- src/components/chartpanels/search.jsx
+++ src/components/chartpanels/search.jsx
@@ -6,31 +6,53 @@ import SearchIcon from "@material-ui/icons/Search"
 import BubbleChartOutlinedIcon from "@material-ui/icons/BubbleChartOutlined"
 import Divider from "@material-ui/core/Divider"
 import ListIcon from "@material-ui/icons/List"
-import Popover from "@material-ui/core/Popover"
 
 export default class SearchPanel extends React.Component {
+  toggleSearch() {
+    document.getElementById("search-icon").classList.add("selected")
+    document.getElementById("sidebar").classList.toggle("collapsed")
+  }
 
-  search () {
-    alert('search clicked');
+  toggleChart(show) {
+    if (show === "chart") {
+      alert("switch to chart view")
+    } else {
+      if (show === "table") {
+        alert("switch to table view")
+      } else {
+        console.log(`Invalid parameter to SearchPanel.toggleChart: ${show}`)
+      }
+    }
   }
 
   render() {
     return (
-      <div className="search-panel">
-        <IconButton onClick={e => this.search()} className="panel-group" aria-label="search">
-          <SearchIcon className="panel-button" />
-        </IconButton>
-        <div className="panel-group">
-          <IconButton aria-label="show bubble chart">
-            <BubbleChartOutlinedIcon className="panel-button" />
+      <div id="sidebar" className="sidebar collapsed">
+        <div className="search-panel">search panel here</div>
+        <div>
+          <IconButton
+            aria-label="search"
+            className="panel-group"
+            onClick={e => this.toggleSearch()}
+          >
+            <SearchIcon id="search-icon" className="panel-button" />
           </IconButton>
-          <Divider variant="middle" className="divider" />
-          <IconButton aria-label="show data table">
-            <ListIcon className="panel-button" />
-          </IconButton>
+          <div className="panel-group">
+            <IconButton
+              aria-label="show bubble chart"
+              onClick={e => this.toggleChart("chart")}
+            >
+              <BubbleChartOutlinedIcon className="panel-button" />
+            </IconButton>
+            <Divider variant="middle" className="divider" />
+            <IconButton
+              aria-label="show data table"
+              onClick={e => this.toggleChart("table")}
+            >
+              <ListIcon className="panel-button" />
+            </IconButton>
+          </div>
         </div>
-        {/* <Popover
-        ></Popover> */}
       </div>
     )
   }
