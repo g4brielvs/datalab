bc904db96a57ee1e1155c51f790c7b39feb9367c
Brett Varney <brett.varney@kc.frb.org>
2019-09-03 13:40:18 -0500

    highlight current button prevent unneeded execution if clicking current button again

--- src/components/chartpanels/search.jsx
+++ src/components/chartpanels/search.jsx
@@ -7,22 +7,43 @@ import BubbleChartOutlinedIcon from "@material-ui/icons/BubbleChartOutlined"
 import Divider from "@material-ui/core/Divider"
 import ListIcon from "@material-ui/icons/List"
 
+const searchButtonId = 'search-icon';
+const chartButtonId = 'chart-icon';
+const tableButtonId = 'list-icon';
+
 export default class SearchPanel extends React.Component {
-  toggleSearch() {
-    document.getElementById("search-icon").classList.add("selected")
-    document.getElementById("sidebar").classList.toggle("collapsed")
+  constructor(props) {
+    super(props);
+    this.state = { activeButton: 0 };
   }
 
-  toggleChart(show) {
-    if (show === "chart") {
-      alert("switch to chart view")
-    } else {
-      if (show === "table") {
-        alert("switch to table view")
+  componentWillUpdate(nextProps, nextState) {
+    if (this.state.activeButton !== nextState.activeButton) {
+      if (this.state.activeButton) {
+        document.getElementById(this.state.activeButton).classList.remove("selected");
+      }
+      document.getElementById(nextState.activeButton).classList.add("selected");
+    }
+  }
+
+  activateButton(buttonId) {
+    if (buttonId === searchButtonId) {
+      this.toggleSearch();
+    }
+    if (buttonId !== this.state.activeButton) {
+      this.setState({ activeButton: buttonId });
+      if (buttonId === chartButtonId) {
+        alert("switch to chart view");
+      } else if (buttonId === tableButtonId) {
+        alert("switch to table view");
       } else {
-        console.log(`Invalid parameter to SearchPanel.toggleChart: ${show}`)
+        console.log(`Invalid parameter to SearchPanel.activateButton: ${buttonId}`);
       }
     }
+  }
+
+  toggleSearch() {
+    document.getElementById("sidebar").classList.toggle("collapsed");
   }
 
   render() {
@@ -33,23 +54,23 @@ export default class SearchPanel extends React.Component {
           <IconButton
             aria-label="search"
             className="panel-group"
-            onClick={e => this.toggleSearch()}
+            onClick={() => this.activateButton(searchButtonId)}
           >
-            <SearchIcon id="search-icon" className="panel-button" />
+            <SearchIcon id={searchButtonId} className="panel-button" />
           </IconButton>
           <div className="panel-group">
             <IconButton
               aria-label="show bubble chart"
-              onClick={e => this.toggleChart("chart")}
+              onClick={e => this.activateButton(chartButtonId)}
             >
-              <BubbleChartOutlinedIcon className="panel-button" />
+              <BubbleChartOutlinedIcon id={chartButtonId} className="panel-button" />
             </IconButton>
             <Divider variant="middle" className="divider" />
             <IconButton
               aria-label="show data table"
-              onClick={e => this.toggleChart("table")}
+              onClick={e => this.activateButton(tableButtonId)}
             >
-              <ListIcon className="panel-button" />
+              <ListIcon id={tableButtonId} className="panel-button" />
             </IconButton>
           </div>
         </div>
