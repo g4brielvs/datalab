40495e335d2a4df842732b4e8fb97b0e0d7715b4
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-09-09 11:55:19 -0400

    wip

---
+++ src/page-sections/story-demo/story-template.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+<section className='follow-up container-fluid'>
+  <div className='row justify-content-center'>
+  <div className='intro col-xs-5' dangerouslySetInnerHTML={{__html: props.sectionTitle}}></div>
+<div className='col-xs-7' dangerouslySetInnerHTML={{__html: props.sectionText }}></div>
+</div>
+</section>
