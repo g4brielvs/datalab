140b37266bca948ce6e2d35f0681a48af76e843c
Brett Varney <brett.varney@kc.frb.org>
2019-09-13 08:30:38 -0500

    use Default super-template remove Default margins, as other pages have different ones

--- src/components/layouts/default/default.jsx
+++ src/components/layouts/default/default.jsx
@@ -1,46 +1,31 @@
-/**
- * Home component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from 'react-helmet'
-
-import SiteHeader from '../../headers/site'
-import PageHeader from '../../headers/page'
-import PageFooter from "../../footers/page"
-import SiteFooter from "../../footers/site"
-import '../../../styles/index.scss'
+import SiteHeader from '../../headers/site';
+import PageHeader from '../../headers/page';
+import PageFooter from '../../footers/page';
+import SiteFooter from '../../footers/site';
 import './default.scss'
-
-const defaultStyle = {
-  margin: '1% 9%'
-}
 
 const Default = ({ children }) => (
   <>
     <Helmet>
-        <title>Data Lab - U.S. Treasury</title>
+      <title>Data Lab - U.S. Treasury</title>
     </Helmet>
 
     <SiteHeader />
     <PageHeader />
-    <main
-      className='default'
-      style = {defaultStyle} >{children}</main>
-
+    <div className='default'>
+      {children}
+    </div>
     <PageFooter />
     <SiteFooter />
   </>
-)
+);
 
 Default.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Default
-
-
+export default Default;
--- src/components/layouts/tool/tool.jsx
+++ src/components/layouts/tool/tool.jsx
@@ -1,56 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './tool.scss'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './tool.scss';
 
 import Grid from '@material-ui/core/Grid';
 
-import SiteHeader from '../../headers/site'
-import PageHeader from '../../headers/page'
-import ShareMenu from '../../share-menu/share-menu'
-import HWCTALink from '../../hwcta-link/hwcta-link'
-import MoreAnalyses from '../../more-analyses/more-analyses'
-import PageFooter from '../../footers/page'
-import SiteFooter from '../../footers/site'
+import Default from '../default/default';
+import ShareMenu from '../../share-menu/share-menu';
+import HWCTALink from '../../hwcta-link/hwcta-link';
+import MoreAnalyses from '../../more-analyses/more-analyses';
 
 class ToolLayout extends React.Component {
-  render = () => <>
-    <SiteHeader />
-    <PageHeader />
-    <div className='tool-page'>
-      <header>
-        <Grid container justify='space-between'>
-          <Grid item className='title'>{this.props.title}</Grid>
-          <Grid item><ShareMenu /></Grid>
-        </Grid>
-      </header>
-      <section>
-        <p className='intro' dangerouslySetInnerHTML={{ __html: this.props.introSentence }}></p>
-        <p dangerouslySetInnerHTML={{ __html: this.props.contextStatement }}></p>
-      </section>
+  render = () =>
+    <Default>
+      <div className='tool-page'>
+        <header>
+          <Grid container justify='space-between'>
+            <Grid item className='title'>{this.props.title}</Grid>
+            <Grid item><ShareMenu /></Grid>
+          </Grid>
+        </header>
+        <section>
+          <p className='intro' dangerouslySetInnerHTML={{ __html: this.props.introSentence }}></p>
+          <p dangerouslySetInnerHTML={{ __html: this.props.contextStatement }}></p>
+        </section>
 
-      <main>
-        <article>
-          <section>
-            {this.props.children}
-          </section>
-          <section className='follow-up'>
-            <Grid container>
-              <Grid item xs={4}>
-                <div className='intro' dangerouslySetInnerHTML={{ __html: this.props.sectionTitle }}></div>
+        <main>
+          <article>
+            <section>
+              {this.props.children}
+            </section>
+            <section className='follow-up'>
+              <Grid container>
+                <Grid item xs={4}>
+                  <div className='intro' dangerouslySetInnerHTML={{ __html: this.props.sectionTitle }}></div>
+                </Grid>
+                <Grid item xs={8}>
+                  <div dangerouslySetInnerHTML={{ __html: this.props.sectionText }}></div>
+                </Grid>
               </Grid>
-              <Grid item xs={8}>
-                <div dangerouslySetInnerHTML={{ __html: this.props.sectionText }}></div>
-              </Grid>
-            </Grid>
-          </section>
-        </article>
-      </main>
-      <HWCTALink url={'#'} />
-      <MoreAnalyses />
-    </div>
-    <PageFooter />
-    <SiteFooter />
-  </>
+            </section>
+          </article>
+        </main>
+        <HWCTALink url={'#'} />
+        <MoreAnalyses />
+      </div>
+    </Default>
 }
 
 
