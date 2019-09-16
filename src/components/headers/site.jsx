bba1531744bca8cae3fe2301fc21b0f757df1a3d
xjackk <xkillilea@gmail.com>
2019-09-16 13:12:56 -0400

    site header start, blue banner done and styled for mobile

--- src/components/headers/site.jsx
+++ src/components/headers/site.jsx
@@ -3,17 +3,44 @@ import "./site.scss"
 
 import flag from "../../../src/images/us_flag_small.png"
 
-const SiteHeader = props => (
-  <header className="site-header">
-    <div>
-      <a href="https://www.usaspending.gov">USAspending.gov</a> |&nbsp;
-      <a href="https://datalab.usaspending.gov">Data Lab</a>
-    </div>
-    <div className="header-statement">
-      An official website of the U.S. government
-      <img src={flag} alt="U.S. flag" />
-    </div>
-  </header>
-)
+class SiteHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // todo
+    };
+  };
+  
+  render() {
+    return(
+      <header class="official-banner">
+	<ul class="official-banner__site-list">
+	  <li class="official-banner__site-item">
+	    <a class="official-banner__site-link" href="https://www.usaspending.gov">
+              USAspending.gov
+	    </a>
+	  </li>
+	  <li class="official-banner__site-item official-banner__site-item_spacer" aria-hidden="true">
+      |
+	  </li>
+	  <li class="official-banner__site-item">
+	    <a class="official-banner__site-link" href="https://datalab.usaspending.gov">
+              Data Lab
+	    </a>
+	  </li>
+	</ul>
+	<div class="official-banner__wrapper">
+	  <div class="official-banner__text">
+	    An official website of the U.S. government
+	  </div>
+	  <img class="official-banner__flag" src={flag} alt="U.S. flag" />
+	</div>
+      </header>
+    );
+  }
+
+}; // end class
+
+
 
 export default SiteHeader
