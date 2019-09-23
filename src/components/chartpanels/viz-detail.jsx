043223c30f39c08100dfcb222183fddb4b926771
Brett Varney <brett.varney@kc.frb.org>
2019-09-23 08:07:42 -0500

    wip content laid out working on show/hide

--- src/components/chartpanels/viz-detail.jsx
+++ src/components/chartpanels/viz-detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Paper } from '@material-ui/core';
+import { Paper, IconButton, Grid } from '@material-ui/core';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import styles from './viz-detail.module.scss';
 
 export default class VizDetailPanel extends React.Component {
@@ -10,8 +11,18 @@ export default class VizDetailPanel extends React.Component {
 
   render = () => (
     <Paper className={styles.details}>
-      {this.props.children}
-    </Paper>);
+      <Grid container>
+        <Grid item>
+          {this.props.children}
+        </Grid>
+        <Grid item>
+          <IconButton className={styles.closeButton}>
+            <HighlightOffIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
 
 VizDetailPanel.propTypes = {
