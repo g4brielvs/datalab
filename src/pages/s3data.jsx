1f82f1fdd35780a8503771adc679be7e554e8b5e
Brett Varney <brett.varney@kc.frb.org>
2019-09-26 08:41:44 -0500

    wip

--- src/pages/s3data.jsx
+++ src/pages/s3data.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
 const AWS = require('aws-sdk');
-// AWS.config.update(
-//   {
-//     accessKeyId: 'data-lab-data/dts/dts.csv'
-//     // secretAccessKey: '.. your secret key ..'
-//   }
-// );
+AWS.config.update(
+  {
+    accessKeyId: 'AKIA3YCOPFO3LMPSH75B',
+    secretAccessKey: 'Y8+RnjHvaGH05c4KZl6C+o6w0JoC5JQbq0K/SFue',
+    region: 'us-gov-west-1'
+  }
+);
 
 let data;
-// const s3 = new AWS.S3();
-// s3.getObject(
-//   { Bucket: 'datalab-qat', Key: 'data-lab-data/dts/dts.csv' },
-//   function (error, data) {
-//     if (error != null) {
-//       alert('Failed to retrieve an object: ' + error);
-//     } else {
-//       alert('Loaded ' + data.ContentLength + ' bytes');
-//       data = data.Body;
-//     }
-//   }
-// );
+const s3 = new AWS.S3();
+s3.getObject(
+  { Bucket: 'datalab-dev', Key: 'data-lab-data/dts/dts.csv' },
+  function (error, data) {
+    if (error != null) {
+      alert('Failed to retrieve an object: ' + error);
+    } else {
+      alert('Loaded ' + data.ContentLength + ' bytes');
+      data = data.Body;
+    }
+  }
+);
 export default class DataPOC extends React.Component {
   render = () => (<>{JSON.stringify(data)}</>);
 }
