7c64447966af83ebc50c94cc73afa9212ee712f3
Brett Varney <brett.varney@kc.frb.org>
2019-09-27 10:59:13 -0500

    wip build css modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,12 +3,30 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+exports.onCreateWebpackConfig = ({ actions, loaders }) => {
   actions.setWebpackConfig({
-    devtool: "eval-source-map"
+    devtool: 'eval-source-map',
   });
+  loaders: [
+    {
+      test: /\.s?[ac]ss$/,
+      loader: 'style-loader'
+    }, {
+      test: /\.s?[ac]ss$/,
+      loader: 'css-loader',
+      query: {
+        modules: true,
+        localIdentName: '[name]__[local]___[hash:base64:5]'
+      }
+    }
+  ]);
+  // plugins: [
+  //   new ExtractTextPlugin('styles.css')
+  // ]
 };
