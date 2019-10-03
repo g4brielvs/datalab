9e2bf6518bec8ace256b82f1521aa7af25497c41
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-10-03 15:36:00 -0400

    mui theme fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,6 @@
 // You can delete this file if you're not using it
 import('normalize-scss');
 import('flexboxgrid');
+
+import muiRootWrapper from "./mui-theme-wrapper"
+export const wrapRootElement = muiRootWrapper
