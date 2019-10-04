290b5923bba8d241705deec8483fab51ed20d497
Brett Varney <brett.varney@kc.frb.org>
2019-10-04 13:02:00 -0500

    fix header top margin

--- src/page-sections/colleges-and-universites/custom-header/custom-header.jsx
+++ src/page-sections/colleges-and-universites/custom-header/custom-header.jsx
@@ -1,27 +1,21 @@
 import './custom-header.scss'
 import React from 'react';
 
-import { Grid } from '@material-ui/core';
-
 const CustomHeader = (props) => (
   <div className='header container-fluid'>
-    <Grid container>
-      <Grid item xs={10}>
-        <section className='header--hero'>
-          <div id='header__banner'>
-            <p className='header__subtext'>
-              {props.subtext}
-            </p>
-            <p className='header__subblurb'>
-              {props.subblurb}
-            </p>
-            <p className='header__blurb'>
-              {props.blurb.map(item => item)}
-            </p>
-          </div>
-        </section>
-      </Grid>
-    </Grid>
+    <section className='header--hero'>
+      <div id='header__banner'>
+        <p className='header__subtext'>
+          {props.subtext}
+        </p>
+        <p className='header__subblurb'>
+          {props.subblurb}
+        </p>
+        <p className='header__blurb'>
+          {props.blurb.map(item => item)}
+        </p>
+      </div>
+    </section>
   </div>
 )
 
--- src/pages/colleges-and-universities/index.jsx
+++ src/pages/colleges-and-universities/index.jsx
@@ -11,16 +11,16 @@ import SEO from "../../components/seo";
 import StoryLayout from "../../components/layouts/story/story"
 
 class CollegesAndUniversitiesPage extends React.Component {
-  render() {
-    return <><StoryLayout isCustomHeader={true}>
-      <SEO title="Colleges and Universities" keywords={[`gatsby`, `application`, `react`]}/>
+  render = () => <>
+    <StoryLayout isCustomHeader={true}>
+      <SEO title="Colleges and Universities" keywords={[`gatsby`, `application`, `react`]} />
 
       <CustomHeader
         subtext={'Federal Investment in Higher Education'}
-        subblurb={['Explore the Federal Investment in your ', <br key="subblurb-linebreak"/>,
+        subblurb={['Explore the Federal Investment in your ', <br key="subblurb-linebreak" />,
           <span key="subblurb-callout" className={'header--red'}>Alma Mater</span>]}
         blurb={['Did you know the federal government invested over $149 billion in colleges and universities in fiscal year 2018?',
-          <br key="blurb-linebreak"/>,
+          <br key="blurb-linebreak" />,
           'Those funds made an impact on over 3,000 schools, approximately 15 million undergraduates, and a little over 2.5 million graduate students.']}
       />
 
@@ -33,39 +33,39 @@ class CollegesAndUniversitiesPage extends React.Component {
             subblurb: "WHAT IS A FEDERAL INVESTMENT?",
             blurb: "Learn more about the three categories of federal investments: student aid, grants, and contracts."
           },
-            {
-              section: "two",
-              number: "02",
-              subtext: "My Alma Mater",
-              subblurb: "How much did my school receive?",
-              blurb: "Search for your school and discover details about federal funding at your alma mater."
-            },
-            {
-              section: "three",
-              number: "03",
-              subtext: "Agency Investment",
-              subblurb: "Which federal agencies are involved?",
-              blurb: "Find out which federal agencies provide investments and in what amounts."
-            },
-            {
-              section: "four",
-              number: "04",
-              subtext: "Investment Categories",
-              subblurb: "What are the investments used for?",
-              blurb: "Discover more about what is funded by federal investment."
-            },
+          {
+            section: "two",
+            number: "02",
+            subtext: "My Alma Mater",
+            subblurb: "How much did my school receive?",
+            blurb: "Search for your school and discover details about federal funding at your alma mater."
+          },
+          {
+            section: "three",
+            number: "03",
+            subtext: "Agency Investment",
+            subblurb: "Which federal agencies are involved?",
+            blurb: "Find out which federal agencies provide investments and in what amounts."
+          },
+          {
+            section: "four",
+            number: "04",
+            subtext: "Investment Categories",
+            subblurb: "What are the investments used for?",
+            blurb: "Discover more about what is funded by federal investment."
+          },
           ]}
       />
 
-      <Overview/>
+      <Overview />
       <Agencies />
       <Categories />
       <Footnotes
         footnotes={['Financial obligations represent outstanding debt or regular payments to another party. A negative value obligation (de-obligation) results from a transaction that lowers the debt amount. A grant or contract has a negative obligation for a given fiscal year when it spans multiple fiscal years and the sum of the transactions for that particular fiscal year was a net reduction of the original obligation.',
-      'Due to the way military academies are funded, they have not been included in this analysis.']} />
+          'Due to the way military academies are funded, they have not been included in this analysis.']} />
     </StoryLayout>
-    </>
-  }
+  </>
+
 }
 
 export default CollegesAndUniversitiesPage
