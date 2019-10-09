da12544c515e504898d923c5107a16c5f823e01a
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-10-09 12:05:30 -0400

    wip

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import styles from "./styles/variables.scss"
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200
+    }
+  }
+});
+
+export default theme;
