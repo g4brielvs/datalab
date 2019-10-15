458ae6b2c6af89c2201427b6e8e9012e7aaf0f81
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-10-15 14:15:17 -0400

    wip

---
+++ src/components/visualizations/vega-example/ReactVegaDemo.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable no-magic-numbers */
+import React from 'react';
+import { Vega, createClassFromSpec } from 'react-vega';
+import data1 from '../../../unstructured-data/vega-example/data1.json';
+import spec1 from '../../../unstructured-data/vega-example/spec1';
+import spec2 from '../../../unstructured-data/vega-example/spec2';
+import sunburstSpec from '../../../unstructured-data/vega-example/sunburst-spec';
+import flare from '../../../unstructured-data/vega-example/flare.json';
+
+
+const BarChart = createClassFromSpec({ spec: sunburstSpec });
+
+const code1 = `<Vega data={this.state.data} spec={this.state.spec} onSignalHover={this.handleHover} />`;
+
+const code2 = `const BarChart = ReactVega.createClassFromSpec(barSpec);
+<BarChart data={this.state.data} onSignalHover={this.handleHover} />`;
+
+export default class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: flare,
+      info: '',
+      spec: sunburstSpec,
+    };
+
+    this.handleHover = this.handleHover.bind(this);
+    this.handleToggleSpec = this.handleToggleSpec.bind(this);
+    this.handleUpdateData = this.handleUpdateData.bind(this);
+    this.handlers = { tooltip: this.handleHover };
+  }
+
+  handleHover(...args) {
+    this.setState({
+      info: JSON.stringify(args),
+    });
+  }
+
+  handleToggleSpec() {
+    const { spec } = this.state;
+    if (spec === spec1) {
+      this.setState({ spec: spec2 });
+    } else {
+      this.setState({ spec: spec1 });
+    }
+  }
+
+  handleUpdateData() {
+    // const tree = [];
+    // for (let i = 1; i <= 20; i += 1) {
+    //   table.push({
+    //     amount: Math.round(Math.random() * 100),
+    //     category: String.fromCharCode(65 + i),
+    //   });
+    // }
+    this.setState({ data: { flare } });
+  }
+
+  render() {
+    const { data, spec, info } = this.state;
+
+    return (
+      <div>
+        <div style={{ float: 'right' }}>
+          <iframe
+            title="star"
+            src="https://ghbtns.com/github-btn.html?user=vega&repo=react-vega&type=star&count=true"
+            frameBorder="0"
+            scrolling="0"
+            width="100px"
+            height="20px"
+          />
+        </div>
+        <button type="button" onClick={this.handleToggleSpec}>
+          Toggle Spec
+        </button>
+        <button type="button" onClick={this.handleUpdateData}>
+          Update data
+        </button>
+        <h3>
+          <code>&lt;Vega&gt;</code> React Component
+        </h3>
+        Will recompile when spec changes and update when data changes.
+        <pre>{code1}</pre>
+        <Vega data={data} spec={spec} signalListeners={this.handlers} />
+        <h3>
+          <code>ReactVega.createClassFromSpec()</code>
+        </h3>
+        Use the given spec to create a reusable component.
+        <pre>{code2}</pre>
+        {/*<BarChart data={data} signalListeners={this.handlers} />*/}
+        <div>
+          Hover info: <code>{info}</code>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/pages/vega-example/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Demo from "../../components/visualizations/vega-example/ReactVegaDemo"
+
+
+export default class ContractExplorerPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render = () => <>
+    <Demo/>
+
+  </>
+
+}
