4339614b4b34058f32b626a5f24a228ff90556b3
Matthew Brown <matthew.brown@kc.frb.org>
2019-10-17 08:40:17 -0500

    Removed unused code

--- src/page-sections/federal-employees/what.jsx
+++ src/page-sections/federal-employees/what.jsx
@@ -6,21 +6,15 @@ import '../../components/share-menu/share-menu.scss'
 import ControlBar from "../../components/control-bar/control-bar"
 import Reset from "../../components/reset/reset"
 import Share from "../../components/share/share"
-import Filter from "../../components/filter/filter"
 import BarChart from "../../components/visualizations/federal-employees/bar-chart/bar-chart"
 
 function What(props) {
-
-  const defaultImageStyle = {
-    marginBottom: "1rem"
-  };
 
   return (
     <>
       <h2>Federal Employee Bar Chart</h2>
       <ControlBar>
         <Reset />
-        <Filter />
         <Share siteUrl='https://datalab-dev.usaspending.gov/federal-employees' pageUrl='federal-employees'
                twitter='#DataLab #Treasury #DataTransparency #USAspending'
                facebook='' reddit='' linkedin='' tumblr='' email=''/>
