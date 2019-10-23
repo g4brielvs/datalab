9d6d81355724313212992abaca2b43fb8875601b
Matthew Brown <matthew.brown@kc.frb.org>
2019-10-23 08:29:28 -0500

    Updated the styling and design of the hwcta page to match the updated mocks.

--- src/components/hwcta/hwcta.jsx
+++ src/components/hwcta/hwcta.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react"
 import {Grid} from '@material-ui/core';
 import Default from '../../components/layouts/default/default';
-import HwctaLink from "../hwcta-link/hwcta-link"
+import Button from '@material-ui/core/Button';
 import MoreAnalyses from "../more-analyses/more-analyses"
 import hwctaStyles from './hwcta.module.scss';
 import SEO from "../seo"
 import ToolLayout from "../layouts/tool/tool"
 import styles from "../layouts/tool/tool.module.scss"
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import StorySection from "../section-elements/story-section/story-section"
+import DataModule from "../visualizations/federal-employees/util/data-module"
 
 
 function HWCTA(props) {
@@ -23,30 +25,10 @@ function HWCTA(props) {
       ]
     }];
    */
-  const categories = props.categories;
-  const categoriesHTML = [];
-  let curCategory = null;
+  const dataSources = props.dataSources || [];
+  const methodologies = props.methodologies || [];
+  const notes = props.notes || [];
   let dataPage = '';
-
-  for(let i = 0, il = categories.length; i < il; i++){
-    curCategory = categories[i];
-    const sourcesHTML = [];
-
-    for(let j = 0, jl = curCategory.sources.length; j < jl; j++){
-      sourcesHTML.push(<li className={hwctaStyles.sources}>{curCategory.sources[j]}</li>);
-    }
-
-    categoriesHTML.push(<Grid item md={12} lg={6}>
-        <h3>{curCategory.name}</h3>
-      </Grid>)
-    categoriesHTML.push(
-      <Grid item md={12} lg={6}>
-        <ul>
-          {sourcesHTML}
-        </ul>
-      </Grid>
-      );
-  }
 
   if(props.location){
     dataPage = props.location.href;
@@ -55,58 +37,112 @@ function HWCTA(props) {
     }
   }
 
-  return (
+  return <>
 
   <Default>
     <div className={hwctaStyles.hwctaPage}>
       <div className={hwctaStyles.container}>
         <header className={styles.header}>
           <Grid container justify='space-between' spacing={4}>
-            {dataPage ? <Grid item xs={12}><a href={dataPage} className={hwctaStyles.backButtonHeader}><span className={hwctaStyles.backButtonHeaderSlash}>/</span>Back to Analysis</a></Grid> : ''}
-            <Grid item xs={12} className={styles.title}>{props.title}</Grid>
+            {dataPage ? <Grid item xs={12}><a href={dataPage} className={hwctaStyles.backButtonHeader}>Back</a></Grid> : ''}
+            <Grid item xs={12} className={hwctaStyles.title}>{props.title}</Grid>
           </Grid>
         </header>
-        <section>
-          <p className={styles.intro}>How we conducted this analysis</p>
-          <p className={hwctaStyles.sectionText}>We conducted this analysis using data from the following sources</p>
-        </section>
-
+        <h1 className={hwctaStyles.pageHeaderText}>Data Sources and Methodologies</h1>
         <section>
           {props.children}
         </section>
-        <section>
-          <Grid container>
-            {categoriesHTML}
-          </Grid>
-        </section>
-        <section>
-          {props.additionalInformation}
-        </section>
+        <div className={hwctaStyles.sourcesSections}>
+          {dataSources ?
+            <section>
+              <h2>
+                Data Sources
+              </h2>
+              <Grid container>
+                {dataSources.map((item, key) => {
+                  return <>
+                    <Grid item md={12} lg={6}>
+                      <h3>{item.name}</h3>
+                    </Grid><Grid item md={12} lg={6}>
+                    <ul>
+                      {item.sources.map((source, key1) => {
+                        return (
+                          <li className={hwctaStyles.sources}>{source}</li>
+                        )
+                      })}
+                    </ul></Grid>
+                  </>
+                })}
+              </Grid>
+            </section>
+            : ''
+          }
+          {methodologies ?
+            <section>
+              <h2>
+                Methodologies
+              </h2>
+              <Grid container>
+                {methodologies.map((item, key) => {
+                  return <>
+                    <Grid item md={12} lg={6}>
+                      <h3>{item.name}</h3>
+                    </Grid><Grid item md={12} lg={6}>
+                    <ul>
+                      {item.sources.map((source, key1) => {
+                        return (
+                          <li className={hwctaStyles.sources}>{source}</li>
+                        )
+                      })}
+                    </ul></Grid>
+                  </>
+                })}
+              </Grid>
+            </section>
+            : ''
+          }
+          {notes ?
+            <section>
+              <h2>
+                Notes
+              </h2>
+              <Grid container>
+                {notes.map((item, key) => {
+                  return <>
+                    <Grid item md={12} lg={6}>
+                      <h3>{item.name}</h3>
+                    </Grid><Grid item md={12} lg={6}>
+                    <ul>
+                      {item.sources.map((source, key1) => {
+                        return (
+                          <li className={hwctaStyles.sources}>{source}</li>
+                        )
+                      })}
+                    </ul></Grid>
+                  </>
+                })}
+              </Grid>
+            </section>
+            : ''
+          }
+        </div>
       </div>
-      <section className={hwctaStyles.footer}>
+      <section className={hwctaStyles.footer} id={hwctaStyles.backButtonFooterSection}>
         <Grid container>
           {dataPage && !props.noFooterBackButton ?
             <Grid item xs={12}>
-              <a href={dataPage} className={hwctaStyles.backButtonFooter}>
-                <div className={hwctaStyles.backButtonFooterChevron}>
-                  <ChevronRightIcon
-                    aria-label='Go back'/>
-                </div>Back to Analysis
-              </a>
+              <Button variant="contained" size="medium" className={hwctaStyles.backButtonFooter}>
+                <a href={dataPage}>
+                  Back
+                </a>
+              </Button>
             </Grid>
             : ''}
         </Grid>
       </section>
-      <section className={hwctaStyles.analysisOffset}>
-        <Grid container>
-          <Grid item xs={12}>
-              <MoreAnalyses />
-          </Grid>
-        </Grid>
-      </section>
     </div>
   </Default>
-  )
+  </>
 }
 
 export default HWCTA;
--- src/pages/competition-in-contracting/methodologies.jsx
+++ src/pages/competition-in-contracting/methodologies.jsx
@@ -5,7 +5,7 @@ import budgetJpg from '../../images/home/budget.jpg';
 function CICHWCTA(props) {
   const title = 'COMPETITION IN CONTRACTING ANALYSIS';
 
-  const categories = [{
+  const dataSources = [{
     name: 'Category1',
     sources: [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
@@ -26,9 +26,62 @@ function CICHWCTA(props) {
       'aaaaaa ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
       'Lorem ipsum dolor'
     ]
-  }]
+  }];
+  const methodologies = [{
+    name: 'Category1',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Ut enim ad minim Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  },{
+    name: 'Category2',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Ut enim ad minim veniam Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  }];
+
+  const notes = [{
+    name: 'Category1',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Ut enim ad minim Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  },{
+    name: 'Category2',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Ut enim ad minim veniam Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  },{
+    name: 'Category3',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'aaaaaa ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  },{
+    name: 'Category4',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Ut enim ad minim veniam Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  },{
+    name: 'Category5',
+    sources: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+      'aaaaaa ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+      'Lorem ipsum dolor'
+    ]
+  }];
+
   return (
-    <HWCTA location={props.location} title={title} categories={categories}>
+    <HWCTA location={props.location} title={title} dataSources={dataSources} methodologies={methodologies} notes={notes}>
       <img className="" src={budgetJpg} alt=""/>
     </HWCTA>
   )
