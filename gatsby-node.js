c8d9271095c6a6d781b8b6a2d4d581dd0be8085e
Brett Varney <brett.varney@kc.frb.org>
2019-10-28 12:14:13 -0500

    add loadable & clean up config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
@@ -10,53 +5,31 @@ require('dotenv').config({
 // window does not exist when we do builds.
 // to account for this, we will check and use null loaders.
 exports.onCreateWebpackConfig = ({ stage, actions, loaders }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new LoadablePlugin()
+    ]
+  });
   if (stage === "build-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
           {
             test: /mapbox-gl/,
-            use: loaders.null(),
+            use: loaders.null()
           },
-        ],
-      },
-    });
-  }
-  actions.setWebpackConfig({
-    devtool: 'eval-source-map',
-    // // this works to process css files, but not scss
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.s?[ac]ss$/,
-    //       loader: 'style-loader'
-    //     }, {
-    //       test: /\.s?[ac]ss$/,
-    //       loader: 'css-loader',
-    //       query: {
-    //         modules: true,
-    //         localIdentName: '[name]__[local]___[hash:base64:5]'
-    //       }
-    //     }
-    //   ]
-    // }
-    module: {
-      rules: [
-        {
-          test: /\.csv$/,
-          loader: 'csv-loader',
-          options: {
-            dynamicTyping: true,
-            header: true,
-            skipEmptyLines: true
+          {
+            test: /\.csv$/,
+            loader: 'csv-loader',
+            options: {
+              dynamicTyping: true,
+              header: true,
+              skipEmptyLines: true
+            }
           }
-        }
-      ]
-    }
-  });
-
-  // plugins: [
-  //   new ExtractTextPlugin('styles.css')
-  // ]
-
+        ]
+      },
+      devtool: 'eval-source-map'
+    })
+  }
 };
