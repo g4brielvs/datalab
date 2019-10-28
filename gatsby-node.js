19778f997ba90c0b3ffd93c5f3280d077af6d454
Brett Varney <brett.varney@kc.frb.org>
2019-10-28 13:22:30 -0500

    fix web pack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,35 +1,39 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
+const LoadablePlugin = require('@loadable/webpack-plugin');
 
 // window does not exist when we do builds.
 // to account for this, we will check and use null loaders.
 exports.onCreateWebpackConfig = ({ stage, actions, loaders }) => {
-  actions.setWebpackConfig({
-    plugins: [
-      new LoadablePlugin()
-    ]
-  });
   if (stage === "build-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
           {
             test: /mapbox-gl/,
-            use: loaders.null()
+            use: loaders.null(),
           },
-          {
-            test: /\.csv$/,
-            loader: 'csv-loader',
-            options: {
-              dynamicTyping: true,
-              header: true,
-              skipEmptyLines: true
-            }
-          }
-        ]
+        ],
       },
-      devtool: 'eval-source-map'
-    })
-  }
-};
+    });
+  };
+  actions.setWebpackConfig({
+    plugins: [
+      new LoadablePlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.csv$/,
+          loader: 'csv-loader',
+          options: {
+            dynamicTyping: true,
+            header: true,
+            skipEmptyLines: true
+          }
+        }
+      ]
+    }
+  });
+}
