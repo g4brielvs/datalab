71fc8df18196ef15b482ebfe60969d6ef60442c7
Matthew Brown <matthew.brown@kc.frb.org>
2019-11-05 15:38:15 -0600

    Finished out converting the desired project files to use scss modules.

--- src/components/headers/dropdown.jsx
+++ src/components/headers/dropdown.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './page.module.scss';
 
 class Dropdown extends React.Component {
   constructor(props) {
@@ -11,8 +12,8 @@ class Dropdown extends React.Component {
   returnActiveList = data => {
     return data.map((item, i) => {
       return (
-        <li className='data-list-li' key={i}>
-          <a className='data-list-a' href={item.link}>{item.name}</a>
+        <li className={styles.li} key={i}>
+          <a className={styles.a} href={item.link}>{item.name}</a>
         </li>
       );
     });
@@ -23,71 +24,71 @@ class Dropdown extends React.Component {
       (() => {
         if (this.props.activeItem == 'Analyses ') {
           return (
-            <div className='data-list' onMouseLeave={this.props.mouseHandle}>
-              <section className='data-list-section data-list-section__analyses'>
-                <h4 className='section-title'>Topical Analyses</h4>
-                <ul className='data-list-ul data-list-ul__analyses'>{this.returnActiveList(this.state.data[0].analyses.slice(0, 3))}</ul>
+            <div className={styles.dataList} onMouseLeave={this.props.mouseHandle}>
+              <section className={`${styles.section} ${styles.analyses}`}>
+                <h4 className={styles.title}>Topical Analyses</h4>
+                <ul className={`${styles.ul} ${styles.ulAnalyses}`}>{this.returnActiveList(this.state.data[0].analyses.slice(0, 3))}</ul>
               </section>
-              <section className='data-list-section data-list-section__analyses'>
-                <h4 className='section-title'>Contract Analyses</h4>
-                <ul className='data-list-ul data-list-ul__analyses'>{this.returnActiveList(this.state.data[0].analyses.slice(3, 6))}</ul>
+              <section className={`${styles.section} ${styles.analyses}`}>
+                <h4 className={styles.title}>Contract Analyses</h4>
+                <ul className={`${styles.ul} ${styles.ulAnalyses}`}>{this.returnActiveList(this.state.data[0].analyses.slice(3, 6))}</ul>
               </section>
-              <section className='data-list-section data-list-section__analyses'>
-                <h4 className='section-title'>Financial Data Visualizations</h4>
-                <ul className='data-list-ul data-list-ul__analyses'>{this.returnActiveList(this.state.data[0].analyses.slice(6, 9))}</ul>
+              <section className={`${styles.section} ${styles.analyses}`}>
+                <h4 className={styles.title}>Financial Data Visualizations</h4>
+                <ul className={`${styles.ul} ${styles.ulAnalyses}`}>{this.returnActiveList(this.state.data[0].analyses.slice(6, 9))}</ul>
               </section>
             </div>
           );
         }
         if (this.props.activeItem == 'DataLab Express ') {
           return (
-            <div className='data-list' onMouseLeave={this.props.mouseHandle}>
-              <section className='data-list-section data-list-section__express'>
-                <ul className='data-list-ul data-list-ul__express'>{this.returnActiveList(this.state.data[1].express)}</ul>
+            <div className={styles.dataList} onMouseLeave={this.props.mouseHandle}>
+              <section className={`${styles.section} ${styles.express}`}>
+                <ul className={`${styles.ul} ${styles.ulExpress}`}>{this.returnActiveList(this.state.data[1].express)}</ul>
               </section>
             </div>
           );
         }
         if (this.props.activeItem == "America's Finance Guide ") {
           return (
-            <div className='data-list' onMouseLeave={this.props.mouseHandle}>
-              <section className='data-list-section'>
-                <h4 className='section-title'>Overview</h4>
-                <ul className='data-list-ul--ffg'>{this.returnActiveList(this.state.data[2].ffg.slice(0, 1))}</ul>
+            <div className={styles.dataList} onMouseLeave={this.props.mouseHandle}>
+              <section className={styles.section}>
+                <h4 className={styles.title}>Overview</h4>
+                <ul className={styles.ulFfg}>{this.returnActiveList(this.state.data[2].ffg.slice(0, 1))}</ul>
               </section>
-              <section className='data-list-section'>
-                <h4 className='section-title'>Revenue</h4>
-                <ul className='data-list-ul--ffg'>{this.returnActiveList(this.state.data[2].ffg.slice(1, 5))}</ul>
+              <section className={styles.section}>
+                <h4 className={styles.title}>Revenue</h4>
+                <ul className={styles.ulFfg}>{this.returnActiveList(this.state.data[2].ffg.slice(1, 5))}</ul>
               </section>
-              <section className='data-list-section'>
-                <h4 className='section-title'>Spending</h4>
-                <ul className='data-list-ul--ffg'>{this.returnActiveList(this.state.data[2].ffg.slice(5, 9))}</ul>
+              <section className={styles.section}>
+                <h4 className={styles.title}>Spending</h4>
+                <ul className={styles.ulFfg}>{this.returnActiveList(this.state.data[2].ffg.slice(5, 9))}</ul>
               </section>
-              <section className='data-list-section'>
-                <h4 className='section-title'>Deficit</h4>
-                <ul className='data-list-ul--ffg'>{this.returnActiveList(this.state.data[2].ffg.slice(9, 12))}</ul>
+              <section className={styles.section}>
+                <h4 className={styles.title}>Deficit</h4>
+                <ul className={styles.ulFfg}>{this.returnActiveList(this.state.data[2].ffg.slice(9, 12))}</ul>
               </section>
-              <section className='data-list-section'>
-                <h4 className='section-title'>Debt</h4>
-                <ul className='data-list-ul--ffg'>{this.returnActiveList(this.state.data[2].ffg.slice(12, 16))}</ul>
+              <section className={styles.section}>
+                <h4 className={styles.title}>Debt</h4>
+                <ul className={styles.ulFfg}>{this.returnActiveList(this.state.data[2].ffg.slice(12, 16))}</ul>
               </section>
             </div>
           );
         }
         if (this.props.activeItem == "Resources ") {
           return (
-            <div className='data-list' onMouseLeave={this.props.mouseHandle}>
-              <section className='data-list-section data-list-section__resources'>
-                <ul className='data-list-ul data-list-ul__resources'>{this.returnActiveList(this.state.data[3].resources)}</ul>
+            <div className={styles.dataList} onMouseLeave={this.props.mouseHandle}>
+              <section className={`${styles.section} ${styles.resources}`}>
+                <ul className={`${styles.ul} ${styles.ulResources}`}>{this.returnActiveList(this.state.data[3].resources)}</ul>
               </section>
             </div>
           );
         }
         if (this.props.activeItem == " Glossary") {
           return (
-            <div className='data-list' onMouseLeave={this.props.mouseHandle}>
-              <section className='data-list-section data-list-section__glossary'>
-                <ul className='data-list-ul data-list-ul__glossary'>{this.returnActiveList(this.state.data[4].glossary)}</ul>
+            <div className={styles.dataList} onMouseLeave={this.props.mouseHandle}>
+              <section className={`${styles.section} ${styles.glossary}`}>
+                <ul className={`${styles.ul} ${styles.ulGlossary}`}>{this.returnActiveList(this.state.data[4].glossary)}</ul>
               </section>
             </div>
           );
