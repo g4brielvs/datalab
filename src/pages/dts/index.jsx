556082ff5ac5c9ce9a2995a22937eccd6e4ab457
Brett Varney <brett.varney@kc.frb.org>
2019-11-05 15:24:14 -0600

    re-lazy-load component

--- src/pages/dts/index.jsx
+++ src/pages/dts/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import ControlBar from '../../components/control-bar/control-bar';
-import DTS from '../../components/visualizations/dts/dts';
 import Grid from '@material-ui/core/Grid';
 import SEO from '../../components/seo';
 import ShareMenu from '../../components/share-menu/share-menu';
 import ToolLayout from "../../components/layouts/tool/tool"
 
-// import loadable from '@loadable/component';
-// const DTS = loadable(() => import(`../../components/visualizations/dts/dts`));
-// import csvData from '../../unstructured-data/dts/dts.csv';
+import loadable from '@loadable/component';
+const DTS = loadable(() => import(`../../components/visualizations/dts/dts`));
+import csvData from '../../unstructured-data/dts/dts.csv';
 
 import AWS from 'aws-sdk';
 AWS.config.update(
