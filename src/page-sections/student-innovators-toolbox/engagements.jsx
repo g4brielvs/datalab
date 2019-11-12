f8686b8a9af83abcefc4fc758c406f718d78cfe3
Matthew Brown <matthew.brown@kc.frb.org>
2019-11-12 15:18:07 -0600

    Initial work on student toolbox.

---
+++ src/page-sections/student-innovators-toolbox/engagements.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from '../../pages/student-innovators-toolbox/student-innovators-toolbox.module.scss';
+
+export default {
+  title: 'AGENCY-RELATED QUESTIONS',
+  introSentence: <></>,
+  data: [
+    {
+      accordionTitles: [
+        <>Use the data to learn about something else</>
+      ],
+      accordionContent: [
+        <p>Quantitative courses at every level require datasets for classroom lessons, homework, problem sets, exams, and more. Courses we believe
+          are a good fit for our data include those that focus on probability, statistics, econometrics, economics, federal budgeting, and data science.</p>,
+        <p>There are two high-level options of USAspending.gov datasets: (1) a prepared, clean dataset and (2) a real-world, raw dataset. Below we explain
+          what we mean by clean and raw, as well as the circumstances that call for each. Both options involve students using Python, R, Stata, SAS,
+          Excel, or some other software to analyze (or clean then analyze) the data.</p>,
+        <h1>
+          Clean dataset
+        </h1>,
+        <p>A prepared and clean dataset is easier to use out of the gate because it is smaller and requires no data cleaning or data wrangling on the
+          student’s part. That is, the data is in the desired format/shape to begin the desired analysis immediately. There are no missing or duplicate
+          values, and the dataset contains a limited number of fields/columns. Prepared and clean datasets are best for teaching other concepts
+          (e.g. regression analysis) when and you don’t want to overwhelm the student with the pre-cleaning steps. They’re also helpful for analysis
+          and data visualization projects.</p>,
+        <h1>
+          Raw dataset
+        </h1>,
+        <p>
+          In more advanced classes, the goal is to help the student understand how data will actually look when they first get it. This may require
+          teaching the student how to handle missing values or de-duplicate records. Often original owners of data have it structured in a way to serve
+          their own business purposes, so the student may be required to change the shape of the data entirely in a processing stage before they can
+          begin any analysis. These types of data sets are useful for students learning data wrangling, data cleaning techniques; those learning more
+          “real-world” data handling skills in order to prep datasets before they apply statistical techniques, or prior to learning different
+          languages typical to data science, e.g. R or Python.</p>
+      ]
+    },
+    {
+      accordionTitles: [<>Use the data to learn about federal budgeting and/or federal spending</>],
+      accordionContent: [<p>While the engagement above focuses on learning skills related to data and analysis, the courses mentioned are often
+        agnostic to the actual content of the dataset. A second option is to use USAspending data to specifically focus its content for courses
+        touching on federal budgeting or federal spending processes. Programs interested in federal government data may include both undergraduate
+        and graduate levels for finance, accounting, public administration, public policy, or business schools.</p>,
+        <p>Similar to above, this engagement includes two options: a prepared, clean dataset or “real-world” raw dataset that requires cleaning
+          and preparation prior to consumption.</p>,
+      <h1>Option 1) Clean dataset</h1>,
+      <p>See Engagement 1, Option 1</p>,
+      <h1>Option 2) Raw dataset</h1>,
+      <p>See Engagement 2, Option 2</p>]
+    },
+    {
+      accordionTitles: [<>Use the data for a consulting or capstone project (given a problem statement or question)</>],
+      accordionContent: [<p>In this engagement, a student or student team uses data from USAspending.gov to identify a real-world question to
+        answer. Using the principles of user-centered design as outlined in the Resources section, the student team identifies a “client”,
+        and the student or team acts as analysts or consultants. The student team conducts initial interviews with the client to determine
+        how USAspending.gov data can help answer a question or solve a problem, and the student team then researches, performs analysis,
+        and provides recommendations in order to answer the question or advise a particular course of action. While this type of structure
+        is common for public policy students, it can easily be applied to a wide variety of settings ranging from business to data science
+        students, or even an interdisciplinary team where a “real-world” project is the goal (e.g. student associations, clubs).</p>,
+        <p>The student team can decide, in consultation with their “client”, to produce a product or visualization instead of an analysis or
+          recommendation. The focus would be less about gathering data to answer a specific question and providing a recommendation, and more
+          about creating a product, tool, visualization for the Data Lab’s website and portfolio. Students with a wide range of backgrounds
+          including, quantitative analysis, graphic design, UX, UI, design, data visualization, data science, computer science, and more can
+          contribute to this type of engagement. Examples of software/languages used for executing tasks or analysis in this engagement are:
+          Tableau, R and R Shiny, Python, Stata, JavaScript, HTML, CSS, D3, and React.</p>]
+    }
+  ]
+};
---
+++ src/pages/student-innovators-toolbox/index.jsx
@@ -0,0 +1,110 @@
+import React, {Component} from "react";
+import styles from './student-innovators-toolbox.module.scss';
+
+/* components */
+import FaqLayout from "../../components/layouts/faq/faq";
+import Downloads from '../../components/section-elements/downloads/downloads';
+import { Hidden, Grid } from '@material-ui/core';
+import Accordion from "../../components/accordion/accordion"
+import DesktopImage from '../../images/analyst-guide/Analyst Desktop.png';
+import TabletImage from '../../images/analyst-guide/Analyst Tablet.png';
+import MobileImage from '../../images/analyst-guide/Analyst Mobile.png';
+
+/* Sections */
+import typesOfEngagement from '../../page-sections/student-innovators-toolbox/engagements';
+
+class AnalystGuidePage extends Component {
+
+  render() {
+    const title = `What is the Analyst's Guide to Federal Spending Data?`;
+    const heroImage = <>
+      <Hidden lgDown>
+        <img src={DesktopImage} className={styles.heroImage}/>
+      </Hidden>
+      <Hidden xlUp xsDown>
+        <img src={TabletImage} className={styles.heroImage}/>
+      </Hidden>
+      <Hidden smUp>
+        <img src={MobileImage} className={styles.heroImage}/>
+      </Hidden>
+    </>;
+
+    const introSentence = <>
+      <p>
+        The federal government uses contracts to buy the things it needs, from office furniture to airplanes. It also uses contracts to buy services
+        ranging from internet to research and development. Federal Agencies and their sub-agencies are responsible for issuing contracts and
+        categorizing the goods and services they purchase using Product and Service Codes (PSC).
+      </p>
+      <p>
+        Although contracts and grants share some similarities, each serves a different purpose. Generally, contracts allow the government ot purchase
+        goods and services directly to the public. For example, the Air Force uses contracts to acquire fighter jets to execute its mission, while the Federal
+        Highway Administration uses grants to provide funds to states for public roads.
+      </p>
+    </>
+    const rawContent = [
+      typesOfEngagement
+    ]
+
+    const content = <>
+      {
+        rawContent.map((section, key) => {
+          return <section key={key} className={styles.section}>
+            <div className={styles.sectionHeader}>
+              {section.title}
+            </div>
+            {
+              section.data.map((datum, key1) => {
+                let accordionTitle = <></>;
+                let accordionContent = <></>;
+                if(datum.accordionTitles && datum.accordionTitles.length){
+                  accordionTitle = <div className={styles.containerNoFlex} key={key1}>
+                    {datum.accordionTitles.map((title, key2) => {
+                      return <div key={key2} className={styles.accordionTitleDiv}>
+                        {title}
+                      </div>
+                    })}
+                  </div>
+                }
+                if(datum.accordionContent && datum.accordionContent.length){
+                  accordionContent =  datum.accordionContent.map((content, key2) => {
+                    return <>
+                      {content}
+                    </>
+                  })
+                }
+                return <Accordion key={key1} title={accordionTitle} containerClass={styles.accordionContainer}>
+                  {accordionContent}
+                </Accordion>
+              })
+            }
+          </section>
+        })
+      }
+    </>
+
+    return <>
+      <div id={styles.analystGuidePage}>
+        <FaqLayout
+          title={title}
+          introSentence={introSentence}
+          heroImage={heroImage}
+          location={this.props.location}
+        >
+          {content}
+          <Hidden lgDown>
+            <Downloads/>
+          </Hidden>
+          <Hidden xlUp>
+            <Grid container justify='center' className={styles.download}>
+              <Grid item xs={12}>
+                <Downloads/>
+              </Grid>
+            </Grid>
+          </Hidden>
+        </FaqLayout>
+      </div>
+    </>
+  }
+}
+
+export default AnalystGuidePage
