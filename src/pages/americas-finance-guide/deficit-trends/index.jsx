5cd20188c14dd0aad1fafd41fced07c6fc353823
petway <brian.petway@kc.frb.org>
2019-12-04 13:59:01 -0600

    da4996 added a class to the accordion heading; recompiled with webpack

--- src/pages/americas-finance-guide/deficit-trends/index.jsx
+++ src/pages/americas-finance-guide/deficit-trends/index.jsx
@@ -55,46 +55,47 @@ function DeficitTrendsPage(props) {
                             text="How has the deficit changed over the past few years? Check out #YourGuide for visualizations. #DataLab #OpenGov"
                             facebook='' reddit='' linkedin='' tumblr='' email='' />
                     </ControlBar>
-                </div>
 
-                <h1>Federal Deficit Trends Over Time</h1>
+                    <h1>Federal Deficit Trends Over Time</h1>
 
-                <img className="deficit-trend" src="/americas-finance-guide/images/deficit-trend.svg" alt="In 2000 and 2001 the federal government experienced surpluses of $237 billion and $127 billion respectively. Since 2001, the federal government has not had another surplus. While the annual deficit did not exceed $500 billion from 2002 to 2008, the annual deficit increased substantially in response to the Great Recession with annual deficits exceeding $1 trillion from 2009 to 2012. The annual deficit declined from 2012 to 2015, falling below the $500 billion mark in 2015. Since then, however, the annual deficit has grown each year to $984 billion in 2019."></img>
+                    <img className="deficit-trend" src="/americas-finance-guide/images/deficit-trend.svg" alt="In 2000 and 2001 the federal government experienced surpluses of $237 billion and $127 billion respectively. Since 2001, the federal government has not had another surplus. While the annual deficit did not exceed $500 billion from 2002 to 2008, the annual deficit increased substantially in response to the Great Recession with annual deficits exceeding $1 trillion from 2009 to 2012. The annual deficit declined from 2012 to 2015, falling below the $500 billion mark in 2015. Since then, however, the annual deficit has grown each year to $984 billion in 2019."></img>
 
-                <aside className="deficit-aside">
-                    <p>Since {AfgData.last_surplus.value}, the U.S. has experienced a deficit each year. Beginning in 2016, increases in spending on Social Security, health care, and interest on federal debt have outpaced the growth of federal revenue.</p>
+                    <aside className="deficit-aside">
+                        <p>Since {AfgData.last_surplus.value}, the U.S. has experienced a deficit each year. Beginning in 2016, increases in spending on Social Security, health care, and interest on federal debt have outpaced the growth of federal revenue.</p>
 
-                    <section className="accordion">
-                        <Accordion title="What does it mean when there is a surplus, balanced budget, and deficit?">
-                            <div className="accordion__content">
-                                <div id="surplusComponent">
-                                    <div className="deficit--surplus__component--content deficit--surplus__surplus">
-                                        <img className="deficit__tabs-image" src="/americas-finance-guide/images/surplus.svg" alt="" role="presentation"></img>
-                                        A surplus occurs when the government collects more money than it spends. The last surplus for the federal government was in {AfgData.last_surplus.value}.
-                                    </div>
-                                    <div className="deficit--surplus__component--content deficit--surplus__balanced">
-                                        <img className="deficit__tabs-image" src="/americas-finance-guide/images/balanceBudget.svg" alt="" role="presentation"></img>
-                                        A balanced budget occurs when the amount the government spends equals the amount the government collects. Sometimes the term balanced budget is used more broadly to refer to instances where there is no deficit.
-                                    </div>
-                                    <div className="deficit--surplus__component--content deficit--surplus__deficit">
-                                        <img className="deficit__tabs-image" src="/americas-finance-guide/images/deficit.svg" alt="" role="presentation"></img>
-                                        A deficit occurs when the government spends more money than it collects. The federal government has run deficits for the last {AfgData.consecutive_deficits.value} years.
+                        <section className="accordion">
+                            <Accordion title="What does it mean when there is a surplus, balanced budget, and deficit?" className="accordion__heading">
+                                <div className="accordion__content">
+                                    <div id="surplusComponent">
+                                        <div className="deficit--surplus__component--content deficit--surplus__surplus">
+                                            <img className="deficit__tabs-image" src="/americas-finance-guide/images/surplus.svg" alt="" role="presentation"></img>
+                                            A surplus occurs when the government collects more money than it spends. The last surplus for the federal government was in {AfgData.last_surplus.value}.
+                                        </div>
+                                        <div className="deficit--surplus__component--content deficit--surplus__balanced">
+                                            <img className="deficit__tabs-image" src="/americas-finance-guide/images/balanceBudget.svg" alt="" role="presentation"></img>
+                                            A balanced budget occurs when the amount the government spends equals the amount the government collects. Sometimes the term balanced budget is used more broadly to refer to instances where there is no deficit.
+                                        </div>
+                                        <div className="deficit--surplus__component--content deficit--surplus__deficit">
+                                            <img className="deficit__tabs-image" src="/americas-finance-guide/images/deficit.svg" alt="" role="presentation"></img>
+                                            A deficit occurs when the government spends more money than it collects. The federal government has run deficits for the last {AfgData.consecutive_deficits.value} years.
+                                        </div>
                                     </div>
                                 </div>
+                            </Accordion>
+                        </section>
+
+                        <section className="tour">
+                            <div className="tour__part-one">
+                                <h1>How does the deficit in the United States compare to other countries?</h1>
+                                <p>When you are done here, see how the U.S. deficit compares to other countries.</p>
                             </div>
-                        </Accordion>
-                    </section>
+                            <a className="tour__link" href="./deficit-country-comparison">Continue<i className="fas fa-chevron-right"></i></a>
+                        </section>
+                    </aside>
 
-                    <section className="tour">
-                        <div className="tour__part-one">
-                            <h1>How does the deficit in the United States compare to other countries?</h1>
-                            <p>When you are done here, see how the U.S. deficit compares to other countries.</p>
-                        </div>
-                        <a className="tour__link" href="./deficit-country-comparison">Continue<i className="fas fa-chevron-right"></i></a>
-                    </section>
-                </aside>
+                    <div className="clearfix"></div>
 
-                <div className="clearfix"></div>
+                </div>
 
                 <section className="hwcta">
                     <Accordion title="Data Sources and Methodology">
