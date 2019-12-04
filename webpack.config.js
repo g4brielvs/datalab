67e9acf32e0ef452ef7202e89b868857da391338
petway <brian.petway@kc.frb.org>
2019-12-04 10:09:07 -0600

    da4996 added new page ; added links to the page in the headers; added the route to it in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,7 @@ module.exports = [ {
     entry: {
         // countryComparison: './citizens-guide/src/deficit/countries/index.js',
         intro: './src/libs/ffg/src/deficit/intro/index.js',
-        // trends: './citizens-guide/src/deficit/trends/index.js'
+        trends: './src/libs/ffg/src/deficit/trends/index.js'
     },
     devtool: devtool,
     devServer: devServer,
