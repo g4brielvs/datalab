59f99b79a7a4db30f12f453db064096412d577af
petway <brian.petway@kc.frb.org>
2019-12-05 10:46:17 -0600

    da4984 commented out the import babel-polyfill that was keeping the page from loading; recompiled

--- src/libs/ffg/src/components/trends/chart.js
+++ src/libs/ffg/src/components/trends/chart.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill';
+// import 'babel-polyfill';
 import { select, selectAll } from 'd3-selection';
 import { scaleLinear } from 'd3-scale';
 import { min, max, range } from 'd3-array';
