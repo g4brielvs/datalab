45190d66388b6c38be358decfd84a2bcf8e70493
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2019-12-06 13:39:20 -0500

    wip

---
+++ src/containers/sunburst-vega-container/sunburst-vega-container.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+import loadable from '@loadable/component'
+import flareData from '../../unstructured-data/contract-explorer/flare.json'
+
+const Sunburst = loadable(() => import(`../../components/visualizations/sunburst-vega/sunburst-vega`))
+
+const SunburstVegaContainer = () => {
+  
+  return <Sunburst data = {flareData} />
+}
+
+export default SunburstVegaContainer
+
+
+
+
+
+
+
