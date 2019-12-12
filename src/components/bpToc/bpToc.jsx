0c17b99b8e445b3daf743ea468ff503da496111c
petway <brian.petway@kc.frb.org>
2019-12-12 10:18:59 -0600

    da4980 added html; corrected the path in the webpack.config; made a react component for bpToc; added two dependencies to package.json; compiled with webpack

--- src/components/bpToc/bpToc.jsx
+++ src/components/bpToc/bpToc.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 const BpToc = () => (
   <>
-    <ul class="toc">
-        <li><a href="#revenue-chapter" class="toc__link scroll-to toc__revenue"><span class="toc__chapter-number">01</span> Federal Revenue</a></li>
-        <li><a href="#spending-chapter" class="toc__link scroll-to toc__spending"><span class="toc__chapter-number">02</span> Federal Spending</a></li>
-        <li><a href="#deficit-chapter" class="toc__link scroll-to toc__deficit"><span class="toc__chapter-number">03</span> Federal Deficit</a></li>
-        <li><a href="#debt-chapter" class="toc__link scroll-to toc__debt"><span class="toc__chapter-number">04</span> Federal Debt</a></li>
+    <ul className="toc">
+        <li><a href="#revenue-chapter" className="toc__link scroll-to toc__revenue"><span className="toc__chapter-number">01</span> Federal Revenue</a></li>
+        <li><a href="#spending-chapter" className="toc__link scroll-to toc__spending"><span className="toc__chapter-number">02</span> Federal Spending</a></li>
+        <li><a href="#deficit-chapter" className="toc__link scroll-to toc__deficit"><span className="toc__chapter-number">03</span> Federal Deficit</a></li>
+        <li><a href="#debt-chapter" className="toc__link scroll-to toc__debt"><span className="toc__chapter-number">04</span> Federal Debt</a></li>
     </ul>
   </>
 )
