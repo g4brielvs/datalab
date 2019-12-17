7321dd0ee8a06de3ec341245fd88c8f31ba29381
petway <brian.petway@kc.frb.org>
2019-12-17 11:01:11 -0600

    da4985 created the page, no html in it yet

---
+++ src/pages/americas-finance-guide/revenue-categories/index.jsx
@@ -0,0 +1,26 @@
+import '../../../libs/ffg/src/globalSass/cg.scss'
+import '../../../libs/ffg/src/globalSass/normalize.scss'
+import '../../../libs/assets/ffg/nav/nav.css'
+import '../../../libs/ffg/src/revenue/categories/revenue-categories.scss'
+
+import React from "react"
+import SEO from "../../../components/seo"
+import AfgData from "../../../libs/_data/object_mapping.yml"
+import Default from "../../../components/layouts/default/default"
+import { HeadOnly } from "../../../components/headers/headers"
+import Accordion from '../../../components/accordion/accordion'
+import ControlBar from '../../../components/control-bar/control-bar'
+import Share from '../../../components/share/share'
+import { Helmet } from 'react-helmet';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight, faAngleDown, faAngleUp, faStreetView } from '@fortawesome/free-solid-svg-icons';
+
+function RevenueCategoriesPage(props) {
+    return (
+        <>
+        </>
+    )
+}
+
+export default RevenueCategoriesPage
