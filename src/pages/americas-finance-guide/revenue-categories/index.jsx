7222a576773ca9597c954a31dcd56fbe9cad5756
petway <brian.petway@kc.frb.org>
2019-12-19 10:29:44 -0600

    da4989 added fa icons for sort and search to both revenue and spending categories pages

--- src/pages/americas-finance-guide/revenue-categories/index.jsx
+++ src/pages/americas-finance-guide/revenue-categories/index.jsx
@@ -68,14 +68,14 @@ function RevenueCategoriesPage(props) {
                                 <div className='bar-controls__left'>
                                     Sort by:
                                     <div className='bar-controls__left--button-div'>
-                                        <button id="sort-amount" className="active">Amount <i className="fas fa-sort-amount-down"></i></button>
-                                        <button id="sort-name">Name <i className="fas fa-sort-alpha-down"></i></button>
+                                        <button id="sort-amount" className="active">Amount <FontAwesomeIcon icon={faSortAmountDown} className="fas fa-sort-amount-down"/></button>
+                                        <button id="sort-name">Name <FontAwesomeIcon icon={faSortAlphaDown} className="fas fa-sort-alpha-down"/></button>
                                     </div>
                                 </div>
                                 <div className='bar-controls__right'>
                                     <div className='bar-controls__right--div'>
                                         <input type="text" placeholder='Search by a term...' id="filter-by-name"></input>
-                                        <i id='filter-by-name-icon' className='fa fa-search spending__search--icon'></i>
+                                        <FontAwesomeIcon icon={faSearch} id='filter-by-name-icon' className="fa fa-search spending__search--icon"/>
                                     </div>
                                  </div>
                             </div>
