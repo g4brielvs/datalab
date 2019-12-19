62f1d5d3b5296c020c66ebee1d17399f0f41dd74
petway <brian.petway@kc.frb.org>
2019-12-19 10:08:26 -0600

    da4989 added the html, Helmet, SEO, Header, Share components

---
+++ src/pages/americas-finance-guide/spending-categories/index.jsx
@@ -0,0 +1,157 @@
+import '../../../libs/ffg/src/globalSass/cg.scss'
+import '../../../libs/ffg/src/globalSass/normalize.scss'
+import '../../../libs/assets/ffg/nav/nav.css'
+import '../../../libs/ffg/src/revenue/categories/revenue-categories.scss'
+
+import React from "react"
+import SEO from "../../../components/seo"
+import AfgData from "../../../libs/_data/object_mapping.yml"
+import Default from "../../../components/layouts/default/default"
+import { HeadOnly } from "../../../components/headers/headers"
+import Accordion from '../../../components/accordion/accordion'
+import ControlBar from '../../../components/control-bar/control-bar'
+import Share from '../../../components/share/share'
+import { Helmet } from 'react-helmet';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+
+function SpendingCategoriesPage(props) {
+    return (
+        <>
+
+            <Helmet>
+                <script defer src="/americas-finance-guide/spending-categories/index.js"></script>
+                <script defer src="/americas-finance-guide/nav.js"></script>
+            </Helmet>
+
+            <SEO
+                title='Data Lab - Federal Spending Categories – U.S. Treasury'
+                description='In 2019, the U.S. government spent $4.4 trillion. Explore the breakdown of federal spending by category or agency.'
+                excerpt='You can explore $4.4 trillion in spending either by category or by agency.  Curious to find out more? Dive deeper to see spending details by sub-category and other options.'
+                keywords={[`federal spending, revenue, federal revenue, U.S. spending, social security, national defense, medicare, health,  interest from debt, veteran’s benefits`]}
+            />
+
+            <Default>
+                <HeadOnly></HeadOnly>
+
+                <nav className="chapter-nav chapter-nav--spending">
+                    <ul className="chapter-nav__primary-list">
+                        <li className="chapter-nav__overview"><a href="../"><FontAwesomeIcon icon={faAngleLeft} className="fas fa-chevron-left"/> Overview</a></li>
+                        <li><a href="./spending-and-gdp">Spending and GDP</a></li>
+                        <li><a href="./spending-categories">Spending Categories</a></li>
+                        <li><a href="./spending-trends">Federal Spending Trends</a></li>
+                        <li><a href="./spending-country-comparison">Country Comparison</a></li>
+                    </ul>
+                    <button className="chapter-nav-trigger">
+                        <FontAwesomeIcon icon={faAngleDown} className="fas fa-lg fa-angle-down menu-down"/>
+                        <FontAwesomeIcon icon={faAngleUp} className="fas fa-lg fa-angle-up menu-up"/>
+                    </button>
+                </nav>
+
+                <div className="ffg-wrapper spending-categories">
+                    <ControlBar>
+                        <Share  
+                            location={props.location}
+                            title="Data Lab - Federal Spending Categories – U.S. Treasury"
+                            twitter="Check out Your Guide to America’s Finances to search federal spending by category or by agency, then download .CSV files with the data to perform your own analysis! #YourGuide #DataLab #OpenGov"
+                            facebook='' reddit='' linkedin='' tumblr='' email='' />
+                    </ControlBar>
+
+                    <h1>Federal Spending by Category and Agency</h1>
+                    <div id="viz-wrapper">
+                        <h2 class="chart-title">
+                            <div id="spending-chart-toggle" class="spending-chart-toggle clearfix" data-active="function">
+                                <span class="spending-chart-toggle__label spending-chart-toggle__label--categories" >Category</span>
+                                <button id="toggle-spending-data-type" class="toggle-control">
+                                    <div class="toggle-control__background">
+                                        <div class="toggle-control__dot"></div>
+                                    </div>
+                                </button>
+                                <span class="spending-chart-toggle__label spending-chart-toggle__label--agency">Agency</span>
+                            </div>
+                        </h2>
+
+                        <div id='viz-chart-container'>
+                            <div id="bar-controls">
+                                <div class='bar-controls__left'>
+                                    Sort by:
+                                    <div class='bar-controls__left--button-div'>
+                                        <button id="sort-amount" class="active">Amount <i class="fas fa-sort-amount-down"></i></button>
+                                        <button id="sort-name">Name <i class="fas fa-sort-alpha-down"></i></button>
+                                    </div>
+                                </div>
+                                <div class='bar-controls__right'>
+                                    <div class='bar-controls__right--div'>
+                                        <input type="text" placeholder='Search by a term...' id="filter-by-name"></input>
+                                        <i id='filter-by-name-icon' class='fa fa-search spending__search--icon'></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr></hr>
+                            <div id="viz"></div>
+                            <hr class='categories__show-more'></hr>
+                            <div id="show-more-container" class='categories__show-more'>
+                                <button id="show-more-button" class="chart__show-more">Show More</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="poc-content" id="income-facts">
+                        <p>In this section, you can explore federal spending either by category or by agency. Curious to find out more? Dive deeper to see spending details by subcategory and other options.</p>
+
+                        <section class="accordion">
+                            <h1 class="accordion__heading">What does the government buy?</h1>
+                            <div class="accordion__content">
+                                <p>Well, a lot. The government buys a variety of products and services used to serve the public &mdash; everything from military aircraft, construction and highway maintenance equipment, buildings, and livestock, to research, education, and training. These purchases are classified by Object Classes, a classification system that describes the types of goods and services purchased by the government.</p>
+                                <p>Our partner site, <a href='{{site.data.object_mapping.usaspending_home.value}}' rel="noopener noreferrer" target="_blank">USASpending.gov</a> allows visitors to explore federal spending by the types of items and services purchased by the federal government. Go explore federal spending by <a href='{{site.data.object_mapping.usaspending_object_class.value}}' rel="noopener noreferrer" target="_blank">Object Class</a>.</p>
+                            </div>
+                        </section>
+
+                        <section class="accordion">
+                            <h1 class="accordion__heading">What are mandatory and discretionary spending?</h1>
+                            <div class="accordion__content">
+                                <p>The difference between mandatory and discretionary spending relates to whether spending is dictated by prior law or voted on in the annual appropriations process. Programs like Social Security, Medicare, and various income security programs, are based on laws previously established that dictate the money budgeted for spending each year, which is why spending for those programs is referred to as mandatory.</p>
+                                <p>Discretionary spending is money formally approved by the President and voted on by Congress during the appropriations process each year. Generally, a majority of the discretionary spending is budgeted towards national defense. The rest of discretionary spending is budgeted to other federal agency programs ranging from transportation, education, housing, social service programs, as well as science and environmental organizations.</p>
+                            </div>
+                        </section>
+
+                        <section class="accordion">
+                            <h1 class="accordion__heading">What does the future of Social Security and Medicare look like?</h1>
+                            <div class="accordion__content">
+                                <p>Each year, the Social Security and Medicare Boards of Trustees publish their <a href="{{site.data.object_mapping.trustees_report.value}}" rel="noopener noreferrer" target="_blank">Annual Report</a> on the financial status of Social Security and Medicare. The Boards’ projections indicate that spending will continue to increase. As the average age of Americans increases, more funding is needed to support entitlement programs like Social Security, Medicare, and retirement and disability services for both military and civil servants. In {{site.data.object_mapping.trustees_fy.value}}, the cost of the Social Security and Medicare programs was {{site.data.object_mapping.ss_med_cost.value}}.</p>
+                                <p>The majority of Social Security and Medicare funding comes from tax revenue and interest on trust fund reserves. For {{site.data.object_mapping.trustees_fy.value}}, income for these programs was {{site.data.object_mapping.ss_med_income.value}}. However, costs are expected to exceed revenue starting in {{site.data.object_mapping.med_deficit_year.value}} for Medicare Part A and {{site.data.object_mapping.ss_deficit_year.value}} for Social Security, and will require the federal government to begin drawing down trust fund balances in order to continue paying full benefits. While Medicare Parts B and D are largely funded by general revenues and beneficiary premiums, the Boards project that Medicare Part A trust fund will be depleted by {{site.data.object_mapping.med_deplete_year.value}} and the Social Security trust fund will be depleted by {{site.data.object_mapping.ss_deplete_year.value}}.</p>
+                            </div>
+                        </section>
+
+                        <section class="tour">
+                            <div class="tour__part-one">
+                                <h1>How has federal spending changed over time?</h1>
+                                <p>When you are done here we encourage you to explore trends in government spending over the past {{site.data.object_mapping.number_trend_years.value}} years.</p>
+                            </div>
+                            <a class="tour__link" href="trends.html">Continue<i class="fas fa-chevron-right"></i></a>
+                        </section>
+                    </div>
+
+                    <section class="hwcta">
+                        <p>
+                            This visualization was created using the <a href="{{site.data.object_mapping.current_mts.value}}" rel="noopener noreferrer" target="_blank">Monthly Treasury Statement (MTS)</a> as the data source for federal government spending of the United States. Some categories from the MTS have been renamed in order to be more easily understood.
+                        </p>
+                        <p>
+                            The Social Security and Medicare Boards of Trustees publish their <a href="{{site.data.object_mapping.trustees_report.value}}" rel="noopener noreferrer" target="_blank">Annual Report</a> in April for the prior Fiscal Year. For example, the 2019 Annual Report contains financial information for the 2018 Fiscal Year. Based on information available at the time of publication, it is believed that the 2019 Annual Report will be available in Fiscal Year 2020.
+                        </p>
+                        <div class="afg__download--div">
+                            <div class="afg__download--heading">Download Source Data</div>
+                            <ul>
+                                <li><a href="../../../assets/ffg/data/federal_spending_categories.csv" download>federal_spending_categories.csv</a></li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
+            </Default>
+        </>
+    )
+}
+
+export default SpendingCategoriesPage
