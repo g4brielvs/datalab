9670336c50acae33dac084dde201f0bdf960a5ee
Matthew Brown <matthew.brown@kc.frb.org>
2019-12-26 13:32:18 -0600

    Updated the styling and design of the hwcta page to match the updated mocks.

--- src/components/section-elements/downloads/downloads.jsx
+++ src/components/section-elements/downloads/downloads.jsx
@@ -6,6 +6,61 @@ import { Grid } from "@material-ui/core";
 
 
 const Downloads = (props) => {
+  function exportToJsonFile(jsonData) {
+    const dataStr = JSON.stringify(jsonData);
+    const dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
+    const exportFileDefaultName = 'data.json';
+
+    return (
+      <a className={downloadsStyles.data}
+         href={dataUri}
+         download={exportFileDefaultName}>
+        <FontAwesomeIcon icon={faDownload} />
+        &nbsp;Download
+      </a>)
+  }
+
+  // Leaving this in just in case we MUST export to a csv file (will talk with Yaw and Andrea when they return), my only reason against JSON to CSV is we lose precision at >15 decimal places.
+  function exportJSONToCsvFile(jsonData) {
+    function parseJSONToCSVStr(jsonData) {
+      if(jsonData.length === 0) {
+        return '';
+      }
+
+      let keys = Object.keys(jsonData[0]);
+
+      let columnDelimiter = ',';
+      let lineDelimiter = '\n';
+
+      let csvColumnHeader = keys.join(columnDelimiter);
+      let csvStr = csvColumnHeader + lineDelimiter;
+
+      jsonData.forEach(item => {
+        keys.forEach((key, index) => {
+          if( (index > 0) && (index < keys.length) ) {
+            csvStr += columnDelimiter;
+          }
+          csvStr += item[key];
+        });
+        csvStr += lineDelimiter;
+      });
+
+      return encodeURIComponent(csvStr);
+    }
+
+    const csvStr = parseJSONToCSVStr(jsonData);
+    const dataUri = 'data:text/csv;charset=utf-8,'+ csvStr;
+    const exportFileDefaultName = 'data.csv';
+
+    return (
+      <a className={downloadsStyles.data}
+         href={dataUri}
+         download={exportFileDefaultName}>
+        <FontAwesomeIcon icon={faDownload} />
+        &nbsp;Download
+      </a>)
+  }
+
   return (
     <Grid
       container
@@ -13,12 +68,16 @@ const Downloads = (props) => {
       justify="flex-end"
       direction="row"
       className={downloadsStyles.download}>
-        {props.date ? <span className={downloadsStyles.fadedModifier}>Updated as of {props.date} / </span> : ''}
+      {props.date ? <span className={downloadsStyles.fadedModifier}>Updated as of {props.date} / </span> : ''}
+      {props.isJSON ?
+        exportToJsonFile(props.data)
+      :
         <a className={downloadsStyles.data}
            href={props.href}>
           <FontAwesomeIcon icon={faDownload} />
           &nbsp;Download
         </a>
+      }
     </Grid>
   )
 }
