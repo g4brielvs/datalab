b10bbb35c0ea942315930906d5333c8d9e89c48b
Matthew Brown <matthew.brown@kc.frb.org>
2020-01-03 09:55:58 -0600

    Fixed issue where Blob was undefined in the build process and our automated builds were failing.

--- src/pages/competition-in-contracting/index.jsx
+++ src/pages/competition-in-contracting/index.jsx
@@ -14,6 +14,7 @@ import ToolLayout from "../../components/layouts/tool/tool";
 import * as d3 from "d3v3"
 import numberFormatter from "../../utils/number-formatter"
 import ControlBar from "../../components/control-bar/control-bar"
+import Downloads from "../../components/section-elements/downloads/downloads"
 
 class CompetitionInContractingPage extends Component {
   constructor(props) {
@@ -233,6 +234,10 @@ class CompetitionInContractingPage extends Component {
                 _svgClass={competitionStyles.svg}
               >
               </Barchart>
+              <Downloads
+                data={this.rawData}
+                isJSON={true}
+              />
             </div>
 
           </Grid>
