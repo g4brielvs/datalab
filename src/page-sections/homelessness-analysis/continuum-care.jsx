f323838f6fba159e41504e8016b7a473dd940b52
Matthew Brown <matthew.brown@kc.frb.org>
2020-01-06 17:48:47 -0600

    Added in treemap with functionality that updates the list of CoCs and the facts on the selected CoC.

--- src/page-sections/homelessness-analysis/continuum-care.jsx
+++ src/page-sections/homelessness-analysis/continuum-care.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react"
+import Treemap from '../../components/visualizations/homelessness-analysis/treemap/treemap';
 
 export default function ContinuumCare(){
   return (
     <>
       <div className="homelessness-subheading">Clustering Administrative Regions</div>
+      <Treemap/>
     </>
   )
 }
