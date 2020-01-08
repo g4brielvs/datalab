6f2616ed43c977e5cb1ab0c715d632527d00593f
xjackk <xkillilea@gmail.com>
2020-01-08 02:05:03 -0500

    styling part one

--- src/components/visualizations/opioid-crisis/sankey/sankeyPopup.jsx
+++ src/components/visualizations/opioid-crisis/sankey/sankeyPopup.jsx
@@ -4,6 +4,7 @@ import Chip from '@material-ui/core/Chip';
 //import Divider from '@material-ui/core/Divider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import { formatNumber } from '../../../../utils/number-formatter';
 
 import leftData from '../../../../unstructured-data/opioid-crisis/sankeyLeftTest.json';
 import rightData from '../../../../unstructured-data/opioid-crisis/sankeyRightTest.json';
@@ -52,10 +53,14 @@ export default class SankeyPopup extends React.Component {
     return societies.map(x => ({society: x.target, value: x.value}));
   };
 
-  findLinks = (agency) => {
-    let subagencies = this.findSubagencies(agency);
+  /*
+    "Follow" an Agency through the graph and get all its Societies (from its said subagencies...)
+   */
+  findLinks = (agency) => { 
+    let subagencies = this.findSubagencies(agency); // finding all the matching guys in "the middle" of the graph...
     let societiesToAgency = [];
 
+    // then we'll loop through all of our guys in the middle and return all the "links" through them.
     subagencies.map(x => {
       let data = this.findSocieties(x.subagency);
       //      return Array.from(new Set(data.map(y => ({linkedSociety: y.society, value: y.value}))));
@@ -63,14 +68,7 @@ export default class SankeyPopup extends React.Component {
         societiesToAgency.push({linkedSociety: y.society, value: y.value});
       });
     });
-
-    //    console.log(this.getUnique(societiesToAgency));
-    // console.log(this.getUnique(societiesToAgency,'linkedSociety'));
-    // console.log((new Set(societiesToAgency)));
-    // console.log(societiesToAgency);
-    
     return this.getUnique(societiesToAgency, 'linkedSociety');
-//    return (societiesToAgency);
   };
 
   setClose = () => {
