61285c2017607c729ed3d1d46062a5cb3ca55806
petway <brian.petway@kc.frb.org>
2020-01-15 13:01:40 -0600

    da5381 removed imports for normalize.css from debt pages that are in this branch

--- src/pages/americas-finance-guide/explore-debt/index.jsx
+++ src/pages/americas-finance-guide/explore-debt/index.jsx
@@ -1,6 +1,5 @@
 import '../../../libs/ffg/src/globalSass/chapterIntroCommon.scss'
 import '../../../libs/ffg/src/globalSass/cg.scss'
-import '../../../libs/ffg/src/globalSass/normalize.scss'
 import '../../../libs/assets/ffg/nav/nav.css'
 
 import React from "react"
