b91ba61e89d7e82908c5bcb58f1900df3171ba0b
petway <brian.petway@kc.frb.org>
2020-01-15 17:17:31 -0600

    da5381 merged dev into this branch; deleted normalize import statements from the two new pages from dev; ran webpackBuild; ran build

--- src/pages/americas-finance-guide/revenue-categories/index.jsx
+++ src/pages/americas-finance-guide/revenue-categories/index.jsx
@@ -1,5 +1,4 @@
 import '../../../libs/ffg/src/globalSass/cg.scss'
-import '../../../libs/ffg/src/globalSass/normalize.scss'
 import '../../../libs/assets/ffg/nav/nav.css'
 import '../../../libs/ffg/src/revenue/categories/revenue-categories.scss'
 
--- webpack.config.js
+++ webpack.config.js
@@ -204,7 +204,7 @@ module.exports = [ {
         intro: './src/libs/ffg/src/debt/intro/index.js',
         trends: './src/libs/ffg/src/debt/trends/index.js',
         analysis: './src/libs/ffg/src/debt/analysis/index.js',
-        // countryComparison: './citizens-guide/src/debt/countries/index.js',
+        countryComparison: './src/libs/ffg/src/debt/countries/index.js',
     },
     devtool: devtool,
     devServer: devServer,
