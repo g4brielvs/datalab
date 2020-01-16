3ace7ddf7e9d785b4242b9ccc38fdb75f584355e
petway <brian.petway@kc.frb.org>
2020-01-16 14:08:59 -0600

    da5499 added wrapper classes to the debt intro and debt country pages

--- src/pages/americas-finance-guide/debt-country-comparison/index.jsx
+++ src/pages/americas-finance-guide/debt-country-comparison/index.jsx
@@ -36,52 +36,54 @@ function DebtCountryComparisonPage(props) {
 
                 <AfgNav location={props.location} chapter={'debt'}></AfgNav>
 
-                <div className="ffg-wrapper">
-                    <ControlBar>
-                        <Share 
-                            location={props.location}
-                            title='Data Lab - Debt Country Comparison – U.S. Treasury'
-                            twitter='How does the federal debt of the U.S. compare to other countries? Check out Your Guide to America’s Finances for data from 169 countries.  Check out the site’s data visualizations, then download .CSV files of the data to perform your own analysis. #YourGuide #DataLab #OpenGov"'
-                            />
-                    </ControlBar>
+                <div className="cg-wrapper country-common-wrapper">
+                    <div className="ffg-wrapper">
+                        <ControlBar>
+                            <Share 
+                                location={props.location}
+                                title='Data Lab - Debt Country Comparison – U.S. Treasury'
+                                twitter='How does the federal debt of the U.S. compare to other countries? Check out Your Guide to America’s Finances for data from 169 countries.  Check out the site’s data visualizations, then download .CSV files of the data to perform your own analysis. #YourGuide #DataLab #OpenGov"'
+                                />
+                        </ControlBar>
 
-                    <h1>Compare the Federal Debt of the United States to Other Countries</h1>
+                        <h1>Compare the Federal Debt of the United States to Other Countries</h1>
 
-                    <div className="country-copy">
-                        <div className="country-copy__text">
-                            <p>How does the United States compare to countries of similar size and gross domestic product? Explore the chart, which shows the total debt of the United States compared to {AfgData.countries_compared.value} other countries listed in the CIA World Factbook. You can compare total debt (in dollars) and debt as a percent of gross domestic product. Find a country of interest and see for yourself. For instance, while the U.S. federal debt was greater than that of China and Japan combined, it ranked {AfgData.compare_us_debt_gdp_rank.value} in debt to gross domestic product. Because the U.S. government has more money coming in and going out than any other country, it helps to compare the debt of the U.S. government to other countries based on the size of their economies. To ensure an accurate comparison, {AfgData.country_compare_year.value} debt data is used in this section, not current fiscal year data.</p>
-                            <p><em>Please note that the countries depicted in this chart have different forms of government, and these differences may impact the scope of finances reported by each country.</em></p>
+                        <div className="country-copy">
+                            <div className="country-copy__text">
+                                <p>How does the United States compare to countries of similar size and gross domestic product? Explore the chart, which shows the total debt of the United States compared to {AfgData.countries_compared.value} other countries listed in the CIA World Factbook. You can compare total debt (in dollars) and debt as a percent of gross domestic product. Find a country of interest and see for yourself. For instance, while the U.S. federal debt was greater than that of China and Japan combined, it ranked {AfgData.compare_us_debt_gdp_rank.value} in debt to gross domestic product. Because the U.S. government has more money coming in and going out than any other country, it helps to compare the debt of the U.S. government to other countries based on the size of their economies. To ensure an accurate comparison, {AfgData.country_compare_year.value} debt data is used in this section, not current fiscal year data.</p>
+                                <p><em>Please note that the countries depicted in this chart have different forms of government, and these differences may impact the scope of finances reported by each country.</em></p>
+                            </div>
+                            <a className="chapter-link chapter-link--revenue" href="/americas-finance-guide/revenue-and-gdp">
+                                <div className="chapter-link__text-block">
+                                    <div className="chapter-link__learn-more">Learn more about</div>
+                                    Federal Revenue
+                                </div>
+                                <FontAwesomeIcon icon={faAngleRight} className="fa fa-angle-right" />
+                            </a>
                         </div>
-                        <a className="chapter-link chapter-link--revenue" href="/americas-finance-guide/revenue-and-gdp">
-                            <div className="chapter-link__text-block">
-                                <div className="chapter-link__learn-more">Learn more about</div>
-                                Federal Revenue
-                            </div>
-                            <FontAwesomeIcon icon={faAngleRight} className="fa fa-angle-right" />
-                        </a>
+
+                        <div className="country-chart">
+                            <h2 className="chart-title">Country Comparison</h2>
+                            <div className="hint">Click <span className="sort-button-placeholder"></span> to sort columns.</div>
+
+                            <div id="viz" className="debt-country"></div>
+                        </div>
+
+                        <div className="clearfix"></div>
+
+                        <section className="hwcta">
+                            <Accordion title="Data Sources and Methodology">
+                                <p>This visualization was created using the <a href={AfgData.country_comparison_mspd.value} rel="noopener noreferrer" target="_blank">Monthly Statement of the Public Debt (MSPD)</a> as the data source for federal government debt of the United States. Gross domestic product (GDP) figures come from the <a href={AfgData.imf_gdp.value} rel="noopener noreferrer" target="_blank">International Monetary Fund (IMF) World Economic Outlook Database (WEOD)</a>. Debt figures for countries other than the United States also come from the <a href={AfgData.imf_debt.value} rel="noopener noreferrer" target="_blank">IMF WEOD</a>. Since debt figures were provided in the national currency for the selected countries, the numbers were subsequently converted to U.S. dollars. Currency conversion rates were pulled from <a href={AfgData.xe_conversion.value} rel="noopener noreferrer" target="_blank">XE.com</a> for {AfgData.xe_conversion_date.value}; the last day of the U.S. federal government's fiscal year.</p>
+                                <p>The conversion of debt figures to U.S. dollars makes comparisons among countries more convenient. However, the implied burden of debt may be misrepresented for a given country if the majority of that nation's debt was denominated in a currency other than U.S. dollars, and the currency in which the debt was held had an abnormal valuation relative to the U.S. dollar on the date of currency conversion.</p>
+                                <div className="afg__download--div">
+                                    <div className="afg__download--heading">Download Source Data</div>
+                                    <ul>
+                                        <li><a href="/americas-finance-guide/afgData/debt_country_comparison.csv" download="debt_country_comparison.csv">debt_country_comparison.csv</a></li>
+                                    </ul>
+                                </div>
+                            </Accordion>
+                        </section>
                     </div>
-
-                    <div className="country-chart">
-                        <h2 className="chart-title">Country Comparison</h2>
-                        <div className="hint">Click <span className="sort-button-placeholder"></span> to sort columns.</div>
-
-                        <div id="viz" className="debt-country"></div>
-                    </div>
-
-                    <div className="clearfix"></div>
-
-                    <section className="hwcta">
-                        <Accordion title="Data Sources and Methodology">
-                            <p>This visualization was created using the <a href={AfgData.country_comparison_mspd.value} rel="noopener noreferrer" target="_blank">Monthly Statement of the Public Debt (MSPD)</a> as the data source for federal government debt of the United States. Gross domestic product (GDP) figures come from the <a href={AfgData.imf_gdp.value} rel="noopener noreferrer" target="_blank">International Monetary Fund (IMF) World Economic Outlook Database (WEOD)</a>. Debt figures for countries other than the United States also come from the <a href={AfgData.imf_debt.value} rel="noopener noreferrer" target="_blank">IMF WEOD</a>. Since debt figures were provided in the national currency for the selected countries, the numbers were subsequently converted to U.S. dollars. Currency conversion rates were pulled from <a href={AfgData.xe_conversion.value} rel="noopener noreferrer" target="_blank">XE.com</a> for {AfgData.xe_conversion_date.value}; the last day of the U.S. federal government's fiscal year.</p>
-                            <p>The conversion of debt figures to U.S. dollars makes comparisons among countries more convenient. However, the implied burden of debt may be misrepresented for a given country if the majority of that nation's debt was denominated in a currency other than U.S. dollars, and the currency in which the debt was held had an abnormal valuation relative to the U.S. dollar on the date of currency conversion.</p>
-                            <div className="afg__download--div">
-                                <div className="afg__download--heading">Download Source Data</div>
-                                <ul>
-                                    <li><a href="/americas-finance-guide/afgData/debt_country_comparison.csv" download="debt_country_comparison.csv">debt_country_comparison.csv</a></li>
-                                </ul>
-                            </div>
-                        </Accordion>
-                    </section>
                 </div>
             </Default>
         </>
