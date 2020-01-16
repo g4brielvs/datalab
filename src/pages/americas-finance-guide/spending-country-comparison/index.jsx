37e4825984880709fbc3372cd709ca168c970de3
petway <brian.petway@kc.frb.org>
2020-01-16 13:43:05 -0600

    da5499 added wrapper classes to the spend intro and spend country pages

--- src/pages/americas-finance-guide/spending-country-comparison/index.jsx
+++ src/pages/americas-finance-guide/spending-country-comparison/index.jsx
@@ -35,60 +35,62 @@ function SpendingCountryComparisonPage(props) {
 
                 <AfgNav location={props.location} chapter={'spending'}></AfgNav>
 
-                <div className="ffg-wrapper">
-                    <ControlBar>
-                        <Share 
-                            location={props.location}
-                            title='Data Lab - Spending Country Comparison – U.S. Treasury'
-                            text='How does U.S. federal spending compare to other countries? Check out Your Guide to America’s Finances for data from 169 countries. #YourGuide #DataLab #OpenGov'
-                            facebook='' reddit='' linkedin='' tumblr='' email='' />
-                    </ControlBar>
+                <div className="cg-wrapper country-common-wrapper">
+                    <div className="ffg-wrapper">
+                        <ControlBar>
+                            <Share 
+                                location={props.location}
+                                title='Data Lab - Spending Country Comparison – U.S. Treasury'
+                                text='How does U.S. federal spending compare to other countries? Check out Your Guide to America’s Finances for data from 169 countries. #YourGuide #DataLab #OpenGov'
+                                facebook='' reddit='' linkedin='' tumblr='' email='' />
+                        </ControlBar>
 
-                    <h1>Compare Federal Spending of the United States to other Countries</h1>
+                        <h1>Compare Federal Spending of the United States to other Countries</h1>
 
-                    <div className="country-copy">
-                        <div className="country-copy__text">
-                            <p>How does the United States compare to countries of similar size and gross domestic product? Explore the chart, which shows the total spending of the United States compared to {AfgData.countries_compared.value} other countries listed in the CIA World Factbook.  You can compare spending (in dollars) and spending as a percent of gross domestic product. Find a country of interest and see for yourself. To ensure an accurate comparison, {AfgData.country_compare_year.value} spending data is used in this section, not current fiscal year data.</p>
-                            <p><em>Please note that the countries depicted in this chart have different forms of government, and these differences may impact the scope of finances reported by each country.</em></p>
+                        <div className="country-copy">
+                            <div className="country-copy__text">
+                                <p>How does the United States compare to countries of similar size and gross domestic product? Explore the chart, which shows the total spending of the United States compared to {AfgData.countries_compared.value} other countries listed in the CIA World Factbook.  You can compare spending (in dollars) and spending as a percent of gross domestic product. Find a country of interest and see for yourself. To ensure an accurate comparison, {AfgData.country_compare_year.value} spending data is used in this section, not current fiscal year data.</p>
+                                <p><em>Please note that the countries depicted in this chart have different forms of government, and these differences may impact the scope of finances reported by each country.</em></p>
+                            </div>
+                            <a className="chapter-link chapter-link--deficit" href="./explore-deficit/">
+                                <div className="chapter-link__text-block">
+                                    <div className="chapter-link__learn-more">Learn more about</div>
+                                    Federal Deficit
+                                </div>
+                                <FontAwesomeIcon icon={faAngleRight} className="fa fa-angle-right" />
+                            </a>
                         </div>
-                        <a className="chapter-link chapter-link--deficit" href="./explore-deficit/">
-                            <div className="chapter-link__text-block">
-                                <div className="chapter-link__learn-more">Learn more about</div>
-                                Federal Deficit
-                            </div>
-                            <FontAwesomeIcon icon={faAngleRight} className="fa fa-angle-right" />
-                        </a>
-                    </div>
 
-                    <div className="country-chart">
-                        <h2 className="chart-title">Country Comparison</h2>
-                        <div className="hint">Click <span className="sort-button-placeholder"></span> to sort columns.</div>
+                        <div className="country-chart">
+                            <h2 className="chart-title">Country Comparison</h2>
+                            <div className="hint">Click <span className="sort-button-placeholder"></span> to sort columns.</div>
 
-                        <div id="viz" className="spending-country"></div>
-                    </div>
+                            <div id="viz" className="spending-country"></div>
+                        </div>
 
-                    <div className="clearfix"></div>
+                        <div className="clearfix"></div>
 
-                    <section className="hwcta">
-                        <Accordion title="Data Sources and Methodology">
-                            <p>The visualization was created using the <a href={AfgData.country_comparison_mts.value} rel="noopener noreferrer" target="_blank">Monthly Treasury Statement (MTS)</a> as the data source for federal government spending of the United States. Gross domestic product (GDP) figures come from the <a href={AfgData.bea_gdp.value} rel="noopener noreferrer" target="_blank">Bureau of Economic Analysis (BEA)</a>. Gross domestic product data for countries other than the United States comes from the <a href={AfgData.imf_gdp.value} rel="noopener noreferrer" target="_blank">International Monetary Fund (IMF) World Economic Outlook Database (WEOD)</a>.</p>
-                            <p>In researching potential data sources for information on the spending of other governments for the country comparison module, we chose the <a href={AfgData.cia_world_factbook.value} rel="noopener noreferrer" target="_blank">CIA World Factbook</a> because it provides the best comparison for the following reasons:</p>
-                            <ul>
-                                <li>the number of countries with {AfgData.country_compare_year.value} data,</li>
-                                <li>relative consistency with the level of government measured (central government only as a standard),</li>
-                                <li>all figures expressed in US dollars</li>
-                            </ul>
-                            <p>Countries with figures before {AfgData.country_compare_year.value} were excluded from the country comparison data set. Although most countries in the data set feature spending from central government sources only, some countries included state/provincial/local spending. As a result, this visualization should not be considered an absolute comparison of the spending of central governments for all countries.</p>
-                            <p>To finish the data set for Country Comparison, gross domestic product figures from BEA and the IMF WEOD were combined with spending figures from the CIA World Factbook data set, excluding countries that did not have data available from both sources.</p>
-                            <div className="afg__download--div">
-                                <div className="afg__download--heading">Download Source Data</div>
+                        <section className="hwcta">
+                            <Accordion title="Data Sources and Methodology">
+                                <p>The visualization was created using the <a href={AfgData.country_comparison_mts.value} rel="noopener noreferrer" target="_blank">Monthly Treasury Statement (MTS)</a> as the data source for federal government spending of the United States. Gross domestic product (GDP) figures come from the <a href={AfgData.bea_gdp.value} rel="noopener noreferrer" target="_blank">Bureau of Economic Analysis (BEA)</a>. Gross domestic product data for countries other than the United States comes from the <a href={AfgData.imf_gdp.value} rel="noopener noreferrer" target="_blank">International Monetary Fund (IMF) World Economic Outlook Database (WEOD)</a>.</p>
+                                <p>In researching potential data sources for information on the spending of other governments for the country comparison module, we chose the <a href={AfgData.cia_world_factbook.value} rel="noopener noreferrer" target="_blank">CIA World Factbook</a> because it provides the best comparison for the following reasons:</p>
                                 <ul>
-                                    <li><a href="/americas-finance-guide/afgData/spending_country_comparison.csv" download="spending_country_comparison.csv">spending_country_comparison.csv</a></li>
+                                    <li>the number of countries with {AfgData.country_compare_year.value} data,</li>
+                                    <li>relative consistency with the level of government measured (central government only as a standard),</li>
+                                    <li>all figures expressed in US dollars</li>
                                 </ul>
-                            </div>
-                        </Accordion>
-                    </section>
-                </div> 
+                                <p>Countries with figures before {AfgData.country_compare_year.value} were excluded from the country comparison data set. Although most countries in the data set feature spending from central government sources only, some countries included state/provincial/local spending. As a result, this visualization should not be considered an absolute comparison of the spending of central governments for all countries.</p>
+                                <p>To finish the data set for Country Comparison, gross domestic product figures from BEA and the IMF WEOD were combined with spending figures from the CIA World Factbook data set, excluding countries that did not have data available from both sources.</p>
+                                <div className="afg__download--div">
+                                    <div className="afg__download--heading">Download Source Data</div>
+                                    <ul>
+                                        <li><a href="/americas-finance-guide/afgData/spending_country_comparison.csv" download="spending_country_comparison.csv">spending_country_comparison.csv</a></li>
+                                    </ul>
+                                </div>
+                            </Accordion>
+                        </section>
+                    </div> 
+                </div>
             </Default>
         </>
     )
