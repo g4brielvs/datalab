b805ca5ccef25de0001fd2cb0e48fac795a90d96
petway <brian.petway@kc.frb.org>
2020-01-16 15:03:45 -0600

    da5499 applied the trendsCommon wrapper class to spend trends, deficit trends, and debt trends pages

--- src/pages/americas-finance-guide/debt-trends/index.jsx
+++ src/pages/americas-finance-guide/debt-trends/index.jsx
@@ -37,57 +37,59 @@ function DebtTrendsPage(props) {
 
                 <AfgNav location={props.location} chapter={'debt'}></AfgNav>
 
-                <div className="ffg-wrapper">
-                    <ControlBar>
-                        <Share 
-                            location={props.location}
-                            title="Data Lab - Federal Debt Trends – U.S. Treasury"
-                            twitter="What is the ratio of debt to GDP? How has the debt changed over time? Your Guide to America’s Finances has data from 2000-2019 and .CSV data files available for download. #YourGuide #DataLab #OpenGov"
-                            />
-                    </ControlBar>
+                <div className="cg-wrapper trends-common-wrapper">
+                    <div className="ffg-wrapper">
+                        <ControlBar>
+                            <Share 
+                                location={props.location}
+                                title="Data Lab - Federal Debt Trends – U.S. Treasury"
+                                twitter="What is the ratio of debt to GDP? How has the debt changed over time? Your Guide to America’s Finances has data from 2000-2019 and .CSV data files available for download. #YourGuide #DataLab #OpenGov"
+                                />
+                        </ControlBar>
 
-                    <h1>Federal Debt Trends Over Time</h1>
+                        <h1>Federal Debt Trends Over Time</h1>
 
-                    <div className="toggle-component toggle-component--debt clearfix">
-                        <span className="toggle-component__label" data-target="debt-image">Debt</span>
-                        <button id="toggle-image" className="toggle-control">
-                            <div className="toggle-control__background">
-                                <div className="toggle-control__dot"></div>
-                            </div>
-                        </button>
-                        <span className="toggle-component__label" data-target="gdp-image">Debt to GDP</span>
-                    </div>
+                        <div className="toggle-component toggle-component--debt clearfix">
+                            <span className="toggle-component__label" data-target="debt-image">Debt</span>
+                            <button id="toggle-image" className="toggle-control">
+                                <div className="toggle-control__background">
+                                    <div className="toggle-control__dot"></div>
+                                </div>
+                            </button>
+                            <span className="toggle-component__label" data-target="gdp-image">Debt to GDP</span>
+                        </div>
 
-                    <div className="trend-chart-container">
-                        <img id="debt-image" className="trend-chart-container__image trend-chart-container__image--active" src="/americas-finance-guide/images/debt-trend.svg" alt="Federal debt steadily rose from $5.7 trillion in 2000 to $10 trillion in 2008. In response to actions taken to address the Great Recession, federal debt increased at a faster rate after 2008. Federal debt rose from $10 trillion in 2008 to $16 trillion in 2012. Federal debt exceeded $20 trillion for the first time in 2017 and reached $22.7 trillion by the end of 2019. "></img>
-                        <img id="gdp-image" className="trend-chart-container__image" src="/americas-finance-guide/images/debt-gdp-ratio-trend.svg" alt="Another way to look at trends in federal debt is to compare it to the size of the economy, as measured by gross domestic product. Federal debt as a percent of gross domestic product in 2000 was 55%. By 2008, this ratio had reached 68%. From 2008 to 2012 federal debt as a percent of gross domestic product increased to 99% and has remained above 100% since 2016. By the end of 2019 the ratio stood at 107%."></img>
-                    </div>
+                        <div className="trend-chart-container">
+                            <img id="debt-image" className="trend-chart-container__image trend-chart-container__image--active" src="/americas-finance-guide/images/debt-trend.svg" alt="Federal debt steadily rose from $5.7 trillion in 2000 to $10 trillion in 2008. In response to actions taken to address the Great Recession, federal debt increased at a faster rate after 2008. Federal debt rose from $10 trillion in 2008 to $16 trillion in 2012. Federal debt exceeded $20 trillion for the first time in 2017 and reached $22.7 trillion by the end of 2019. "></img>
+                            <img id="gdp-image" className="trend-chart-container__image" src="/americas-finance-guide/images/debt-gdp-ratio-trend.svg" alt="Another way to look at trends in federal debt is to compare it to the size of the economy, as measured by gross domestic product. Federal debt as a percent of gross domestic product in 2000 was 55%. By 2008, this ratio had reached 68%. From 2008 to 2012 federal debt as a percent of gross domestic product increased to 99% and has remained above 100% since 2016. By the end of 2019 the ratio stood at 107%."></img>
+                        </div>
 
-                    <aside className="debt-aside">
-                        <p>The U.S. has steadily increased the federal debt since {AfgData.debt_trend_start.value}. Another way to view the federal debt over time is to look at the ratio of federal debt related to gross domestic product (GDP). This ratio has also increased over time. According to the most recent <a href={AfgData.financial_report.value} rel="noopener noreferrer" target="_blank">Financial Report of the United States Government (FR)</a>, the current fiscal policy is not sustainable.</p>
+                        <aside className="debt-aside">
+                            <p>The U.S. has steadily increased the federal debt since {AfgData.debt_trend_start.value}. Another way to view the federal debt over time is to look at the ratio of federal debt related to gross domestic product (GDP). This ratio has also increased over time. According to the most recent <a href={AfgData.financial_report.value} rel="noopener noreferrer" target="_blank">Financial Report of the United States Government (FR)</a>, the current fiscal policy is not sustainable.</p>
 
-                        <section className="tour">
-                            <div className="tour__part-one">
-                                <h1>What else is important to know about the federal debt?</h1>
-                                <p>When you are done here, continue on for a breakdown of the federal debt.</p>
-                            </div>
-                            <a className="tour__link" href="/americas-finance-guide/debt-analysis">Continue<FontAwesomeIcon icon={faAngleRight} className="fa fa-angle-right" /></a>
+                            <section className="tour">
+                                <div className="tour__part-one">
+                                    <h1>What else is important to know about the federal debt?</h1>
+                                    <p>When you are done here, continue on for a breakdown of the federal debt.</p>
+                                </div>
+                                <a className="tour__link" href="/americas-finance-guide/debt-analysis">Continue<FontAwesomeIcon icon={faAngleRight} className="fa fa-angle-right" /></a>
+                            </section>
+                        </aside>
+
+                        <div className="clearfix"></div>
+
+                        <section className="hwcta">
+                            <Accordion title="Data Sources and Methodology">
+                                <p>This visualization was created using the <a href={AfgData.mspd_homepage.value} rel="noopener noreferrer" target="_blank">Monthly Statement of the Public Debt (MSPD)</a> as the data source for federal debt of the United States. <GdpTemplate></GdpTemplate> Throughout this page, we use the gross domestic product for the Fiscal Year, not the Calendar Year, in order to facilitate an appropriate comparison.</p>
+                                <div className="afg__download--div">
+                                    <div className="afg__download--heading">Download Source Data</div>
+                                    <ul>
+                                        <li><a href="/americas-finance-guide/afgData/federal_debt_trends.csv" download="federal_debt_trends.csv">federal_debt_trends.csv</a></li>
+                                    </ul>
+                                </div>
+                            </Accordion>
                         </section>
-                    </aside>
-
-                    <div className="clearfix"></div>
-
-                    <section className="hwcta">
-                        <Accordion title="Data Sources and Methodology">
-                            <p>This visualization was created using the <a href={AfgData.mspd_homepage.value} rel="noopener noreferrer" target="_blank">Monthly Statement of the Public Debt (MSPD)</a> as the data source for federal debt of the United States. <GdpTemplate></GdpTemplate> Throughout this page, we use the gross domestic product for the Fiscal Year, not the Calendar Year, in order to facilitate an appropriate comparison.</p>
-                            <div className="afg__download--div">
-                                <div className="afg__download--heading">Download Source Data</div>
-                                <ul>
-                                    <li><a href="/americas-finance-guide/afgData/federal_debt_trends.csv" download="federal_debt_trends.csv">federal_debt_trends.csv</a></li>
-                                </ul>
-                            </div>
-                        </Accordion>
-                    </section>
+                    </div>
                 </div>
             </Default>
         </>
