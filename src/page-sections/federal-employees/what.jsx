93bd0d1f76e6fc412c54fafe881ff134fcdc7f20
Brett Varney <brett.varney@kc.frb.org>
2020-01-23 12:55:49 -0600

    coorect url for who & what

--- src/page-sections/federal-employees/what.jsx
+++ src/page-sections/federal-employees/what.jsx
@@ -1,25 +1,18 @@
-import React from "react"
-import "../../styles/index.scss"
+import React from 'react';
+import '../../styles/index.scss';
 
-/* components */
-import ControlBar from "../../components/control-bar/control-bar"
-import Reset from "../../components/reset/reset"
-import Share from "../../components/share/share"
-import BarChart from "../../components/visualizations/federal-employees/bar-chart/bar-chart"
+import ControlBar from '../../components/control-bar/control-bar';
+import Reset from '../../components/reset/reset';
+import Share from '../../components/share/share';
+import BarChart from '../../components/visualizations/federal-employees/bar-chart/bar-chart';
 
-function What(props) {
-
-  return (
-    <>
-      <h2>Federal Employee Bar Chart</h2>
-      <ControlBar>
-        <Reset />
-        <Share siteUrl='https://datalab-dev.usaspending.gov/federal-employees' pageUrl='federal-employees'
-               facebook='' reddit='' linkedin='' tumblr='' email=''/>
-      </ControlBar>
-      <BarChart sectionId={props.sectionId} dataSource={props.dataSource}/>
-    </>
-  )
+export default function What(props) {
+  return <>
+    <h2>Federal Employee Bar Chart</h2>
+    <ControlBar>
+      <Reset />
+      <Share siteUrl='https://datalab.usaspending.gov/' pageUrl='federal-employees' facebook='' reddit='' linkedin='' tumblr='' email='' />
+    </ControlBar>
+    <BarChart sectionId={props.sectionId} dataSource={props.dataSource} />
+  </>;
 }
-
-export default What
--- src/page-sections/federal-employees/who.jsx
+++ src/page-sections/federal-employees/who.jsx
@@ -1,28 +1,17 @@
-import React from "react"
-import "../../styles/index.scss"
+import React from 'react';
+import '../../styles/index.scss';
 
-/* components */
-import ControlBar from "../../components/control-bar/control-bar"
-import Share from "../../components/share/share"
-import Treemap from "../../components/visualizations/federal-employees/treemap/treemap"
+import ControlBar from '../../components/control-bar/control-bar';
+import Share from '../../components/share/share';
+import Treemap from '../../components/visualizations/federal-employees/treemap/treemap';
 
-function Who(props) {
-
-  const defaultImageStyle = {
-    marginBottom: "1rem"
-  };
-
-  return (
-    <>
-      <h2>Spending by Agency</h2>
-      <ControlBar>
-        <Share siteUrl='https://datalab-dev.usaspending.gov/' pageUrl='federal-employees'
-                   facebook='' reddit='' linkedin='' tumblr='' email=''/>
-      </ControlBar>
-      <Treemap sectionId={props.sectionId} dataSource={props.dataSource}/>
-    </>
-  )
+export default function Who(props) {
+  return <>
+    <h2>Spending by Agency</h2>
+    <ControlBar>
+      <Share siteUrl='https://datalab.usaspending.gov/' pageUrl='federal-employees' facebook='' reddit='' linkedin='' tumblr='' email='' />
+    </ControlBar>
+    <Treemap sectionId={props.sectionId} dataSource={props.dataSource} />
+  </>;
 }
-
-export default Who
 
