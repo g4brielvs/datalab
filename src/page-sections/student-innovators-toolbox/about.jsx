7ab8c64aef03a3e5aeec1f75d864fff4859a25d6
Brett Varney <brett.varney@kc.frb.org>
2020-02-20 09:10:55 -0600

    add link, fix another

--- src/page-sections/student-innovators-toolbox/about.jsx
+++ src/page-sections/student-innovators-toolbox/about.jsx
@@ -12,12 +12,14 @@ export default {
       accordionContent: [
         <ul className={styles.numberedList}>
           <li>
-            <a href='https://datalab.usaspending.gov/assets/analyst-guide-1-2.pdf' target='_blank'
-               rel='noopener noreferrer'>Analyst Guide to Federal Spending Data</a>
+            <a href='https://datalab.usaspending.gov/assets/analyst-guide-1-2.pdf' target='_blank' rel='noopener noreferrer'>
+              Analyst Guide to Federal Spending Data
+            </a>
           </li>
           <li>
-            <a href='https://www.fiscal.treasury.gov/fsservices/gov/data-trans/dt-daims.htm' target='_blank'
-               rel='noopener noreferrer'>Data Dictionary</a>
+            <a href='https://www.usaspending.gov/#/download_center/data_dictionary' target='_blank' rel='noopener noreferrer'>
+              Data Dictionary
+            </a>
           </li>
         </ul>
       ]
