a6f456266d3bad03480b8a32b1139b6d6ea777ed
Matthew Brown <matthew.brown@kc.frb.org>
2020-02-21 10:25:17 -0600

    Added noreferrer noopener to other email link, removed target=_blank attribute on email links as in Chrome the page flashes quickly to make the change.

--- src/components/footers/page.jsx
+++ src/components/footers/page.jsx
@@ -34,7 +34,7 @@ const PageFooter = props => (
       <div className={pageFooterStyles.flexItem}>
         <div className={pageFooterStyles.title}>Contact Us</div>
         <p>For general inquiries or questions on Data Lab activities or operations, please contact:</p>
-        <p><a href='mailto: usaspending.help@fiscal.treasury.gov?subject=Data Lab - Contact Us'>
+        <p><a href='mailto: usaspending.help@fiscal.treasury.gov?subject=Data Lab - Contact Us' rel="noopener noreferrer">
           E: usaspending.help@fiscal.treasury.gov
         </a></p>
       </div>
@@ -42,7 +42,7 @@ const PageFooter = props => (
         <div className={pageFooterStyles.title}>Mailing List</div>
         <p>
           To join our mailing list, send a blank email with no subject to:{" "}
-          <a href="mailto: datalab@lists.fiscal.treasury.gov" target="_blank" rel="noopener noreferrer">datalab@lists.fiscal.treasury.gov</a>
+          <a href="mailto: datalab@lists.fiscal.treasury.gov" rel="noopener noreferrer">datalab@lists.fiscal.treasury.gov</a>
         </p>
         <div className={pageFooterStyles.title}>Join the Conversation</div>
         <p>
