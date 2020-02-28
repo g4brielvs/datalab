5222bb22238f143f2070fe12a5789e28ca2d6dc0
Brett Varney <brett.varney@kc.frb.org>
2020-02-28 12:41:17 -0600

    use z-index to place radio buttons above chart div

--- src/containers/competition-in-contracting/competition-in-contracting-container.jsx
+++ src/containers/competition-in-contracting/competition-in-contracting-container.jsx
@@ -143,7 +143,7 @@ export class CompetitionInContractingContainer extends Component {
             </Grid>
           </Grid>
 
-          <div>
+          <div className={competitionStyles.legendDiv}>
             <div className={competitionStyles.legend} id={competitionStyles.notCompeted}>
               <svg height='15' width='15'>
                 <circle cx='7.5' cy='7.5' r='7.5' />
@@ -154,8 +154,7 @@ export class CompetitionInContractingContainer extends Component {
               <svg height='15' width='15'>
                 <circle cx='7.5' cy='7.5' r='7.5' />
               </svg>
-              <label htmlFor={competitionStyles.competed}
-                style={{ fontSize: '0.75rem', verticalAlign: 'bottom', padding: '0px 0 0 .1rem' }}>Competed</label>
+              <label htmlFor={competitionStyles.competed} style={{ fontSize: '0.75rem', verticalAlign: 'bottom', padding: '0px 0 0 .1rem' }}>Competed</label>
             </div>
             <div className={competitionStyles.barChartToolbar}>
               <div className={competitionStyles.selectWrapper1}>
