da34a9dcda0ac84724efd4ed809e84ebe6b451b2
Brett Varney <brett.varney@kc.frb.org>
2020-03-10 11:01:28 -0500

    wip gotta switch branches

--- static/americas-finance-guide/revenue/trends.js
+++ static/americas-finance-guide/revenue/trends.js
@@ -19544,12 +19544,11 @@ function getTransform(d3Selection) {
 
 function establishContainer(height, width, accessibilityAttrs) {
   var viz = d3.select('#viz');
-  var svg = viz.select('svg.main'); // console.log('svg.size: ', svg.size());
-  // console.log('width: ', width);
+  var svg = viz.select('svg.main');
 
   if (svg.size() === 0) {
     height = height || 400;
-    width = width || 1200; // console.log('width: ', width);
+    width = width || 1200;
 
     svg = viz.append('svg').classed('main', true).attr('shape-rendering', 'geometricPrecision').attr('height', height).attr('width', width);
   } else if (height) {
@@ -19559,7 +19558,7 @@ function establishContainer(height, width, accessibilityAttrs) {
   if (accessibilityAttrs && Object.keys(accessibilityAttrs).length) {
     svg.append('desc').attr('id', 'svgMainDesc').text(accessibilityAttrs.desc);
     svg.attr('aria-describedby', 'svgMainDesc');
-  } // console.log('svg', svg);
+  }
 
 
   return svg;
