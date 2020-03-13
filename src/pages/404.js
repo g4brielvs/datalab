f21fd78fa3f72e4795dd8b26711dd518b7cb4aa1
Andrea Blackwell <blackwellandrea.bah@gmail.com>
2020-03-13 11:20:16 -0400

    fixing 404 redirect on all pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -43,36 +43,38 @@ const afgTranslation = old => {
 const NotFoundPage = () => {
 
   // check for Jekyll links, redirect if possible
-  if (typeof window !== 'undefined') {
-    let urlFrags;
-    if (window.location.href.indexOf('.html') > -1) {
-      urlFrags = splitUrl(window.location.href);
+	const browser = typeof window !== "undefined";
 
-      let afg;
-      if ((afg = urlFrags[0].indexOf('/americas-finance-guide/')) > -1) { // AFG pages have moved; can't simply strip ".html"
+  if (browser && window.location.href.indexOf('.html') > -1) {
+      const urlFrags = splitUrl(window.location.href);
+      const afg = urlFrags[0].indexOf('/americas-finance-guide/');
+
+      if (afg > -1) { // AFG pages have moved; can't simply strip ".html"
         window.location = (afgTranslation(urlFrags[0].slice(afg + 24)) + (urlFrags[1] ? '?' + urlFrags[1] : ''));
-        return <></>;
       }
       window.location = urlFrags[0] + (urlFrags[1] ? '?' + urlFrags[1] : '');
       return <></>;
-    }
-  }
 
-  return <HeaderOnly _containerClass='four-oh-four' _headerClass='four-oh-four__header'>
-    <SEO title='404: Not found' />
-    <div id='four-oh-four'>
-      <div className='four-container'>
-        <h1>Oops!</h1>
-        <h4>Looks like you got lost in the data.
-            <div><span>It happens.</span> Find your way back.</div>
-        </h4>
-        <div id='btn-container'>
-          <div id='back-btn'><a href='/'>BACK</a></div>
-          <div id='home-btn'><a href='/'>HOME</a></div>
-        </div>
-      </div>
-    </div>
-  </HeaderOnly>
-}
+  } else {
+		return (
+			browser && (<HeaderOnly _containerClass='four-oh-four' _headerClass='four-oh-four__header'>
+					<SEO title='404: Not found'/>
+					<div id='four-oh-four'>
+						<div className='four-container'>
+							<h1>Oops!</h1>
+							<h4>Looks like you got lost in the data.
+								<div><span>It happens.</span> Find your way back.</div>
+							</h4>
+							<div id='btn-container'>
+								<div id='back-btn'><a href='/'>BACK</a></div>
+								<div id='home-btn'><a href='/'>HOME</a></div>
+							</div>
+						</div>
+					</div>
+				</HeaderOnly>
+			)
+		)
+	}
+};
 
 export default NotFoundPage;
