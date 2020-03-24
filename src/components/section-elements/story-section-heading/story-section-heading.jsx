ca887a564fedef365f6987b3db98d0d0f7ddae8a
Brett Varney <brett.varney@kc.frb.org>
2020-03-24 10:14:36 -0500

    wip

--- src/components/section-elements/story-section-heading/story-section-heading.jsx
+++ src/components/section-elements/story-section-heading/story-section-heading.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import storySectionHeadingStyles from "./story-section-heading.module.scss";
+import React from 'react';
+import storySectionHeadingStyles from './story-section-heading.module.scss';
+import './story-section-heading.scss';
 import { Grid } from '@material-ui/core';
 
 const StorySectionHeading = (props) => {
   return (
     <header>
       <section className={storySectionHeadingStyles.header}>
-        <Grid
-          container>
+        <Grid container>
           <Grid item xs={2} sm={12} xl={1} className={storySectionHeadingStyles.headerNumber}>
             <h2>{props.number}</h2>
           </Grid>
@@ -16,10 +16,8 @@ const StorySectionHeading = (props) => {
           </Grid>
         </Grid>
         <Grid container spacing={10} className={storySectionHeadingStyles.introContainer}>
-          <Grid item xs={12} xl={5} className='intro'>{props.teaser}</Grid>
-          <Grid item xs={12} xl={7}>
-            {props.blurb}
-          </Grid>
+          <Grid item xs={12} xl={5} className={storySectionHeadingStyles.intro}>{props.teaser}</Grid>
+          <Grid item xs={12} xl={7}>{props.blurb}</Grid>
         </Grid>
       </section>
     </header>
