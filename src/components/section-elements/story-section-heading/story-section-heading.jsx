52286edde6a7b64cf3abbacc440469a85fe8fe4b
Brett Varney <brett.varney@kc.frb.org>
2020-03-25 11:39:23 -0500

    adjustments

--- src/components/section-elements/story-section-heading/story-section-heading.jsx
+++ src/components/section-elements/story-section-heading/story-section-heading.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import storySectionHeadingStyles from './story-section-heading.module.scss';
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 
 const StorySectionHeading = (props) => {
   return (
@@ -10,7 +10,9 @@ const StorySectionHeading = (props) => {
           <Grid item xs={1} sm={12} xl={1} className={storySectionHeadingStyles.headerNumber}>
             <h2>{props.number}</h2>
           </Grid>
-          <Grid item xs={1} />
+          <Hidden smUp>
+            <Grid item xs={1} />
+          </Hidden>
           <Grid item xs={10} sm={12} xl={11} className={storySectionHeadingStyles.headerTitle}>
             {props.title}
           </Grid>
